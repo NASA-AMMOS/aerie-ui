cc477aaea12753fcd0a5b8cc4b8bfbaeb1fd9d86
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-02-10 11:53:24 -0800

    refactor: add helmet and health to server
    - Add helmet to server for setting safe HTTP headers
    - Add health endpoint for checking server health and uptime

--- server/main.js
+++ server/main.js
@@ -1,11 +1,20 @@
 // @ts-check
 
 const express = require('express');
+const helmet = require('helmet');
 
 function main() {
   const app = express();
   const port = 80;
+
+  app.use(helmet({ contentSecurityPolicy: false }));
   app.use(express.static('public'));
+
+  app.get('/health', (_, res) => {
+    const uptimeMinutes = process.uptime() / 60;
+    res.json({ status: 'healthy', uptimeMinutes });
+  });
+
   app.listen(port, () => {
     console.log(`🚀 aerie-ui-server listening on port ${port}`);
   });
