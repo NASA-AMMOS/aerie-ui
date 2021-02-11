768e371c58aa2e5e22c5f2ef2da9680b752c7d3e
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-02-10 17:27:26 -0800

    refactor: add cam endpoints to server

--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 // @ts-check
+/* eslint-disable @typescript-eslint/naming-convention */
 
 const { readFileSync } = require('fs');
+const { CamApi } = require('@gov.nasa.jpl.aerie/cam');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const fastGlob = require('fast-glob');
@@ -8,11 +11,41 @@ const helmet = require('helmet');
 
 function main() {
   const app = express();
+  const camApi = new CamApi();
   const port = 80;
 
   app.use(helmet({ contentSecurityPolicy: false }));
   app.use(cors());
+  app.use(bodyParser.json());
   app.use(express.static('public'));
+
+  app.post('/cam/login', async (req, res) => {
+    const { body } = req;
+    const { username, password } = body;
+    const response = await camApi.login(username, password);
+    res.json(response);
+  });
+
+  app.post('/cam/logout', async (req, res) => {
+    const { body } = req;
+    const { ssoToken } = body;
+    const response = await camApi.logout(ssoToken);
+    res.json(response);
+  });
+
+  app.post('/cam/session', async (req, res) => {
+    const { body } = req;
+    const { ssoToken } = body;
+    const response = await camApi.session(ssoToken);
+    res.json(response);
+  });
+
+  app.post('/cam/user', async (req, res) => {
+    const { body } = req;
+    const { ssoToken } = body;
+    const response = await camApi.user(ssoToken);
+    res.json(response);
+  });
 
   app.get('/health', (_, res) => {
     const date = new Date().toISOString();
