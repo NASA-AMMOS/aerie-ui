454ee06c799f84f746d1ebbd7db101ce5894054c
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-10-07 14:28:37 -0700

    refactor: start adding timeline form elements

--- src/components/timeline/TimelineForm.svelte
+++ src/components/timeline/TimelineForm.svelte
@@ -3,10 +3,28 @@
 <script lang="ts">
   import Field from '../form/Field.svelte';
   import Panel from '../ui/Panel.svelte';
-  import { selectedTimeline } from '../../stores/views';
+  import {
+    selectedLayerId,
+    selectedLayer,
+    selectedRowId,
+    selectedRow,
+    selectedTimelineId,
+    selectedTimeline,
+    view,
+  } from '../../stores/views';
   import Card from '../ui/Card.svelte';
   import FieldInputText from '../form/FieldInputText.svelte';
   import { required } from '../../utilities/validators';
+  import Label from '../form/Label.svelte';
+  import Select from '../form/Select.svelte';
+
+  function updateRow(prop: string, value: any) {
+    view.updateRow($selectedTimelineId, $selectedRowId, prop, value);
+  }
+
+  function updateTimeline(prop: string, value: any) {
+    view.updateTimeline($selectedTimelineId, prop, value);
+  }
 </script>
 
 <Panel hideHeader hideFooter>
@@ -20,6 +38,7 @@
             type="number"
             value={$selectedTimeline.marginLeft}
             validators={[required]}
+            on:change={e => updateTimeline('marginLeft', e.detail)}
           >
             Margin Left
           </FieldInputText>
@@ -29,11 +48,79 @@
             type="number"
             value={$selectedTimeline.marginRight}
             validators={[required]}
+            on:change={e => updateTimeline('marginRight', e.detail)}
           >
             Margin Right
           </FieldInputText>
+
+          <Field>
+            <Label for="rows">Rows</Label>
+            <Select bind:value={$selectedRowId} name="rows">
+              {#each $selectedTimeline.rows as row}
+                <option value={row.id}>
+                  {row.id}
+                </option>
+              {/each}
+            </Select>
+          </Field>
         {:else}
           <Card class="p-1">No timeline selected</Card>
+        {/if}
+      </details>
+    </Field>
+
+    <Field>
+      <details open>
+        <summary>Row</summary>
+        {#if $selectedRow !== null}
+          <FieldInputText
+            name="timeline-row-height"
+            type="number"
+            value={$selectedRow.height}
+            validators={[required]}
+            on:change={e => updateRow('height', e.detail)}
+          >
+            Height
+          </FieldInputText>
+
+          <Field>
+            <Label for="layers">Layers</Label>
+            <Select bind:value={$selectedLayerId} name="layers">
+              {#each $selectedRow.layers as layer}
+                <option value={layer.id}>
+                  {layer.id}
+                </option>
+              {/each}
+            </Select>
+          </Field>
+        {:else}
+          <Card class="p-1">No row selected</Card>
+        {/if}
+      </details>
+    </Field>
+
+    <Field>
+      <details open>
+        <summary>Layer</summary>
+        {#if $selectedLayer !== null}
+          <Field>
+            <Label for="layer-type">Layer Type</Label>
+            <Select name="layer-type" value={$selectedLayer.type}>
+              <option value="activity"> Activity </option>
+              <option value="resource"> Resource </option>
+            </Select>
+          </Field>
+
+          <Field>
+            <Label for="layer-type">Chart Type</Label>
+            <Select name="layer-type" value={$selectedLayer.chartType}>
+              <option value="activity"> Activity </option>
+              <option value="line"> Line </option>
+              <option value="x-range"> X-Range </option>
+            </Select>
+          </Field>
+        {:else}
+          <Card class="p-1">No layer selected</Card>
         {/if}
       </details>
     </Field>
