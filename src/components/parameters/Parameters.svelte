7e02363191f1b4dad17261b582bcab667e471e38
Chris Camargo <Christopher.A.Camargo@jpl.nasa.gov>
2021-10-19 12:24:50 -0700

    refactor: param label column width
    - Make param label column width a function
    of the elements client width
    - See https://jira.jpl.nasa.gov/browse/AERIE-1447

--- src/components/parameters/Parameters.svelte
+++ src/components/parameters/Parameters.svelte
@@ -9,17 +9,18 @@
   export let disabled: boolean = false;
   export let formParameters: FormParameter[] = [];
 
-  let labelColumnWidth: number = 250;
+  let clientWidth: number;
   let level: number = 0;
   let levelPadding: number = 20;
 
+  $: labelColumnWidth = clientWidth * 0.65;
   $: sortedFormParameters = formParameters.sort((a, b) =>
     compare(a.name, b.name),
   );
 </script>
 
 {#each sortedFormParameters as formParameter (formParameter.name)}
-  <div class="parameter">
+  <div bind:clientWidth class="parameter">
     {#if formParameter.schema.type === 'series' || formParameter.schema.type === 'struct'}
       <ParameterRec
         {disabled}
