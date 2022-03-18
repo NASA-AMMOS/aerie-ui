aec26d96fb73868937111f5a248705ac50b68f7c
camargo <camargo.cac@gmail.com>
2022-03-17 17:23:20 -0700

    ci: create test.yml workflow
    - Add docker-compose-test.yml for running Aerie services
    - Fix routes tests to account for auth turned off
    - Add proper Playwright reporters

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,18 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  reporter: [
+    [process.env.CI ? 'github' : 'list'],
+    ['html', { open: 'never', outputFolder: 'test-report' }],
+  ],
   use: {
     baseURL: 'http://localhost:3000',
     browserName: 'chromium',
   },
   webServer: {
     command: 'ORIGIN=http://localhost:3000 node build',
+    env: {
+      AUTH_TYPE: 'none',
+    },
     port: 3000,
   },
 };
--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -1,30 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test.describe(
-  'Private routes should not be accessible when unauthenticated',
-  () => {
-    test.beforeEach(async ({ context }) => {
-      await context.clearCookies();
-    });
-
-    test('/ redirects to /login', async ({ baseURL, page }) => {
-      await page.goto('/');
-      await expect(page).toHaveURL(`${baseURL}/login`);
-    });
-
-    test('/models redirects to /login', async ({ baseURL, page }) => {
-      await page.goto('/models');
-      await expect(page).toHaveURL(`${baseURL}/login`);
-    });
-
-    test('/plans redirects to /login', async ({ baseURL, page }) => {
-      await page.goto('/plans');
-      await expect(page).toHaveURL(`${baseURL}/login`);
-    });
-
-    test('/plans/0 redirects to /login', async ({ baseURL, page }) => {
-      await page.goto('/plans/0');
-      await expect(page).toHaveURL(`${baseURL}/login`);
-    });
-  },
-);
+test('/ redirects to /plans', async ({ baseURL, page }) => {
+  await page.goto('/');
+  await expect(page).toHaveURL(`${baseURL}/plans`);
+});
