8b2ec1c18a3f13f6749ac2a3226884c6daa1038c
camargo <camargo.cac@gmail.com>
2022-04-04 09:49:20 -0700

    test: set explicit test directory
    - Also never open HTML reports (on fail)

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,14 @@
 const config = {
   reporter: [
     [process.env.CI ? 'github' : 'list'],
-    ['html', { outputFile: 'index.html', outputFolder: 'test-results' }],
+    [
+      'html',
+      { open: 'never', outputFile: 'index.html', outputFolder: 'test-results' },
+    ],
     ['json', { outputFile: 'test-results/json-results.json' }],
     ['junit', { outputFile: 'test-results/junit-results.xml' }],
   ],
+  testDir: './tests',
   use: {
     baseURL: 'http://localhost:3000',
     browserName: 'chromium',
