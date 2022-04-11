372c2026a2efc5c8c3e9261c413fcbc98b7d4757
camargo <camargo.cac@gmail.com>
2022-04-11 13:31:59 -0700

    test: plans page tests
    - Add more plans page tests for plan creation form
    - Add select/option selector helpers
    - Imporve plans page fixture

--- tests/e2e/plans-page.test.ts
+++ tests/e2e/plans-page.test.ts
@@ -42,4 +42,48 @@ test.describe('Plans Page', () => {
     const { text } = await plansPage.selectedModel();
     expect(text).toEqual(modelsPage.modelName);
   });
+
+  test('Create plan button should be disabled after only entering a name', async ({ plansPage }) => {
+    await plansPage.fillInputName();
+    await expect(plansPage.createButton).toBeDisabled();
+  });
+
+  test('Create plan button should be disabled after only entering a start time', async ({ plansPage }) => {
+    await plansPage.fillInputStartTime();
+    await expect(plansPage.createButton).toBeDisabled();
+  });
+
+  test('Create plan button should be disabled after only entering an end time', async ({ plansPage }) => {
+    await plansPage.fillInputEndTime();
+    await expect(plansPage.createButton).toBeDisabled();
+  });
+
+  test('Entering an invalid start time should display an error, and the create button should be disabled', async ({
+    plansPage,
+  }) => {
+    await plansPage.inputStartTime.fill('2022-');
+    await plansPage.inputStartTime.evaluate(e => e.blur());
+    await expect(plansPage.inputStartTime).toHaveClass(/error/);
+    await expect(plansPage.createButton).toBeDisabled();
+  });
+
+  test('Entering an invalid end time should display an error, and the create button should be disabled', async ({
+    plansPage,
+  }) => {
+    await plansPage.inputEndTime.fill('2022-');
+    await plansPage.inputEndTime.evaluate(e => e.blur());
+    await expect(plansPage.inputEndTime).toHaveClass(/error/);
+    await expect(plansPage.createButton).toBeDisabled();
+  });
+
+  test('Create button should be enabled after selecting a model, entering a name, entering a start time, and entering an end time ', async ({
+    modelsPage,
+    plansPage,
+  }) => {
+    await plansPage.selectInputModel(modelsPage.modelName);
+    await plansPage.fillInputName();
+    await plansPage.fillInputStartTime();
+    await plansPage.fillInputEndTime();
+    await expect(plansPage.createButton).not.toBeDisabled();
+  });
 });
--- tests/utils/selectors.ts
+++ tests/utils/selectors.ts
@@ -1,6 +1,37 @@
 import { Page } from '@playwright/test';
 
-export async function getSelect(page: Page, selectSelector: string): Promise<{ text: string; value: string }> {
+export type SelectOption = { text: string; value: string };
+
+export async function getAllSelectOptions(page: Page, selectSelector: string): Promise<SelectOption[]> {
+  return await page.$eval(selectSelector, (el: HTMLSelectElement) => {
+    const options: SelectOption[] = [];
+
+    if (el?.children) {
+      for (const child of el.children) {
+        if (child.nodeName === 'OPTION') {
+          const option = child as HTMLOptionElement;
+          options.push({ text: option.text, value: option.value });
+        }
+      }
+    }
+
+    return options;
+  });
+}
+
+export async function getOptionValueFromText(page: Page, selectSelector: string, text: string): Promise<string> {
+  const options = await getAllSelectOptions(page, selectSelector);
+
+  for (const option of options) {
+    if (option.text === text) {
+      return option.value;
+    }
+  }
+
+  return '';
+}
+
+export async function getSelectedOption(page: Page, selectSelector: string): Promise<SelectOption> {
   const value = await page.$eval(selectSelector, (el: HTMLSelectElement) => el.value);
   const optionSelector = `${selectSelector} > option[value="${value}"]`;
   const text = await page.$eval(optionSelector, (el: HTMLOptionElement) => el.text);
