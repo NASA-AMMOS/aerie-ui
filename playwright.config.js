a922f35e0bd5fe624985e25b40ee6f12c4f63819
Chris <camargo.cac@gmail.com>
2022-04-12 10:45:04 -0700

    test: create and delete plan, config, and renaming (#14)
    - Add create and delete plan tests
    - Add better Playwright config:
    forbid 'only' in CI, retry on fail and record trace/video
    - Update models page fixture to include focus/blur
    events for input field consistance
    - Raname utils to utilities for directory name consistance
    - Add test:dev package.json script to run built app for test codegen

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,19 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  forbidOnly: !!process.env.CI,
   reporter: [
     [process.env.CI ? 'github' : 'list'],
     ['html', { open: 'never', outputFile: 'index.html', outputFolder: 'test-results' }],
     ['json', { outputFile: 'test-results/json-results.json' }],
     ['junit', { outputFile: 'test-results/junit-results.xml' }],
   ],
+  retries: 1,
   testDir: './tests',
   use: {
     baseURL: 'http://localhost:3000',
     browserName: 'chromium',
+    trace: 'on-first-retry',
+    video: 'on-first-retry',
   },
   webServer: {
     command: 'node build',
