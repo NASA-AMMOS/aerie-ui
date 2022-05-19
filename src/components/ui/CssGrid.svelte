0be240f7abaa1660de0de8042984150070b1273d
camargo <camargo.cac@gmail.com>
2022-05-18 17:13:23 -0700

    feat: add CssGridGutter component
    - If CssGrid has a CssGridGutter child, make it resizeable by default

--- src/components/ui/CssGrid.svelte
+++ src/components/ui/CssGrid.svelte
@@ -1,4 +1,10 @@
+<svelte:options immutable={true} />
+
 <script lang="ts">
+  import type { SplitInstance } from 'split-grid';
+  import Split from 'split-grid';
+  import { onMount, tick } from 'svelte';
+
   export { className as class };
   export let padding: string = '0';
   export let columns: string = 'none';
@@ -6,9 +12,42 @@
   export let rows: string = 'none';
 
   let className: string = '';
+  let div: HTMLDivElement;
+  let split: SplitInstance;
+
+  onMount(() => {
+    if (div) {
+      setSplit();
+    }
+  });
+
+  async function setSplit() {
+    await tick();
+
+    if (split) split.destroy();
+    split = Split({});
+
+    if (div) {
+      const columnGutters = div.querySelectorAll<HTMLDivElement>(':scope > .css-grid-gutter.column');
+      const rowGutters = div.querySelectorAll<HTMLDivElement>(':scope > .css-grid-gutter.row');
+
+      for (const columnGutter of Array.from(columnGutters)) {
+        const track = columnGutter.getAttribute('data-track');
+        const trackAsNumber = parseFloat(track);
+        split.addColumnGutter(columnGutter, trackAsNumber);
+      }
+
+      for (const rowGutter of Array.from(rowGutters)) {
+        const track = rowGutter.getAttribute('data-track');
+        const trackAsNumber = parseFloat(track);
+        split.addRowGutter(rowGutter, trackAsNumber);
+      }
+    }
+  }
 </script>
 
 <div
+  bind:this={div}
   class={className}
   style="gap: {gap}; grid-template-columns: {columns}; grid-template-rows: {rows}; padding: {padding}"
 >
---
+++ src/components/ui/CssGridGutter.svelte
@@ -0,0 +1,26 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  export let track: number = 1;
+  export let type: 'column' | 'row' = 'column';
+</script>
+
+<div class="css-grid-gutter {type}" data-track={track} />
+
+<style>
+  .css-grid-gutter {
+    background-color: #eee;
+    background-position: 50%;
+    background-repeat: no-repeat;
+  }
+
+  .css-grid-gutter.column {
+    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==');
+    cursor: col-resize;
+  }
+
+  .css-grid-gutter.row {
+    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAFAQMAAABo7865AAAABlBMVEVHcEzMzMzyAv2sAAAAAXRSTlMAQObYZgAAABBJREFUeF5jOAMEEAIEEFwAn3kMwcB6I2AAAAAASUVORK5CYII=');
+    cursor: row-resize;
+  }
+</style>
