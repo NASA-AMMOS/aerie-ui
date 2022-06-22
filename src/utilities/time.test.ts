7a282c87dc265e3776d54d535a2c5d5eddf9c1fc
Bryan <bryan.duran@jpl.nasa.gov>
2022-06-22 16:58:26 -0700

    [AERIE-1502] feat: display human readable format in duration input (#41)
    - added time functions for converting to/from duration string to microseconds
    - added tests for new duration conversion functions

--- src/utilities/time.test.ts
+++ src/utilities/time.test.ts
@@ -1,7 +1,21 @@
 import { expect, test } from 'vitest';
-import { getDoy } from './time';
+import { convertDurationStringToUs, convertUsToDurationString, getDoy } from '../../src/utilities/time';
 
 test('getDoy', () => {
   const doy = getDoy(new Date('1/3/2019'));
   expect(doy).toEqual(3);
+});
+
+test('convertDurationStringToUs', () => {
+  expect(convertDurationStringToUs('2y 318d 6h 16m 19s 200ms 0us')).toEqual(90577779200000);
+  expect(convertDurationStringToUs('200ms 0us')).toEqual(200000);
+  expect(convertDurationStringToUs('30s')).toEqual(3e7);
+
+  expect(() => convertDurationStringToUs('30f')).toThrowError('Must be of format: 1y 3d 2h 24m 35s 18ms 70us');
+});
+
+test('convertUsToDurationString', () => {
+  expect(convertUsToDurationString(90577779200000)).toEqual('2y 318d 6h 16m 19s 200ms 0us');
+  expect(convertUsToDurationString(200000)).toEqual('0y 0d 0h 0m 0s 200ms 0us');
+  expect(convertUsToDurationString(3e7)).toEqual('0y 0d 0h 0m 30s 0ms 0us');
 });
