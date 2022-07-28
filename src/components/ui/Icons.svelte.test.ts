27a7cebe1c007853328a2b36ed4c448f1a8cf988
Aaron Plave <aaron.plave@jpl.nasa.gov>
2022-07-28 10:18:25 -0700

    build: add sveltekit-svg Vite plugin to import SVGs (#78)
    * Add sveltekit-svg vite plugin to import SVGs
    * Add unit test for importing SVGs as Svelte components

---
+++ src/components/ui/Icons.svelte.test.ts
@@ -0,0 +1,16 @@
+import ActivityIcon from '@nasa-jpl/stellar/icons/svg/plan.svg?component';
+import { cleanup, render } from '@testing-library/svelte';
+import { afterEach, describe, expect, it } from 'vitest';
+
+describe('SVG Components', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('Should render an imported SVG', () => {
+    const { container } = render(ActivityIcon);
+
+    expect(container.getElementsByTagName('svg').length).to.equal(1);
+    expect(container.getElementsByClassName('st-icon').length).to.equal(1);
+  });
+});
