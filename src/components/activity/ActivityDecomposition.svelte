70fceff8a1931984b2a4b92856d0e2439581fb72
Aaron Plave <aaron.plave@jpl.nasa.gov>
2022-07-28 13:58:35 -0700

    Style and safety updates to ActivityDecomposition

--- src/components/activity/ActivityDecomposition.svelte
+++ src/components/activity/ActivityDecomposition.svelte
@@ -6,50 +6,63 @@
   import TreeLeafIcon from '@nasa-jpl/stellar/icons/svg/tree_leaf.svg?component';
   import TreeParentCollapsedIcon from '@nasa-jpl/stellar/icons/svg/tree_parent_collapsed.svg?component';
   import TreeParentExpandedIcon from '@nasa-jpl/stellar/icons/svg/tree_parent_expanded.svg?component';
-  import { activitiesMap } from '../../stores/activities';
+  import { activitiesMap, selectedActivityId } from '../../stores/activities';
 
   export let selected_id: number | null = null;
   export let expanded = true;
   export let id: number = 0;
 
   $: activity = $activitiesMap[id];
-  $: isRoot = !activity.parent_id;
-  $: type = activity.type || '';
-  $: child_ids = activity.child_ids || null;
+  $: isRoot = activity ? !activity.parent_id : true;
+  $: type = activity?.type || '';
+  $: child_ids = activity?.child_ids || null;
   $: hasChildren = child_ids ? child_ids.length > 0 : false;
+  $: role = isRoot ? 'tree' : 'treeitem';
+  $: nodeClass =
+    'activity-decomposition-node activity-decomposition-' +
+    (id === selected_id ? 'selected st-typography-medium' : 'unselected st-typography-body');
+  $: buttonClass = 'st-button icon' + (!hasChildren ? ' st-button-no-hover' : '');
 
   function toggle() {
     expanded = !expanded;
   }
 </script>
 
-<div class="activity-decomposition" on:click={toggle}>
-  {#if isRoot}
-    {#if expanded}
-      <CaretDownIcon />
-    {:else}
-      <CaretRightIcon />
-    {/if}
-  {:else if hasChildren}
-    {#if expanded}
-      <TreeParentExpandedIcon />
-    {:else}
-      <TreeParentCollapsedIcon />
-    {/if}
-  {:else}
-    <TreeLeafIcon />
-  {/if}
-  <span class={id === selected_id ? 'st-typography-medium' : 'unselected st-typography-body'}>{type}</span>
-</div>
+{#if !activity}
+  <div class="activity-decomposition activity-decomposition-not-found st-typography-medium" {role}>
+    Activity {id} not found
+  </div>
+{:else}
+  <div class="activity-decomposition" {role}>
+    <button class={buttonClass} on:click={toggle}>
+      {#if isRoot}
+        {#if expanded}
+          <CaretDownIcon />
+        {:else}
+          <CaretRightIcon />
+        {/if}
+      {:else if hasChildren}
+        {#if expanded}
+          <TreeParentExpandedIcon />
+        {:else}
+          <TreeParentCollapsedIcon />
+        {/if}
+      {:else}
+        <TreeLeafIcon />
+      {/if}
+    </button>
+    <span on:click={() => ($selectedActivityId = id)} on:dblclick={toggle} class={nodeClass}>{type}</span>
+  </div>
 
-{#if hasChildren && expanded}
-  <ul>
-    {#each child_ids as child_id}
-      <li>
-        <svelte:self id={$activitiesMap[child_id]?.id} {selected_id} />
-      </li>
-    {/each}
-  </ul>
+  {#if hasChildren && expanded}
+    <ul>
+      {#each child_ids as child_id}
+        <li>
+          <svelte:self id={$activitiesMap[child_id]?.id} {selected_id} />
+        </li>
+      {/each}
+    </ul>
+  {/if}
 {/if}
 
 <style>
@@ -63,13 +76,55 @@
   }
 
   .activity-decomposition {
+    cursor: default;
     height: 24px;
     display: flex;
     align-items: center;
-    gap: 4px;
+    /* gap: 4px; */
+    position: relative;
   }
 
-  .unselected {
+  .activity-decomposition-not-found {
+    cursor: pointer;
+    color: var(--st-red);
+  }
+
+  .activity-decomposition-unselected {
     color: var(--st-gray-60);
+  }
+
+  .activity-decomposition-selected {
+    background: #c2e0fc;
+  }
+
+  .activity-decomposition-node {
+    display: flex;
+    flex: 1;
+    height: inherit;
+    align-items: center;
+    padding-left: 4px;
+    border-radius: 2px;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .activity-decomposition-node:hover:not(.activity-decomposition-selected) {
+    background: var(--st-gray-10);
+  }
+
+  .activity-decomposition-node {
+    display: flex;
+    flex: 1;
+    height: inherit;
+    align-items: center;
+    padding-left: 4px;
+    border-radius: 2px;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .st-button-no-hover:hover {
+    background: none;
+    cursor: default;
   }
 </style>
