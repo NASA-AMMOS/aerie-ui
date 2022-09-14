e56519fcf63e55437dd3d5b37ab2f85dfeaeaedb
Jeff Pamer <jeffrey.r.pamer@jpl.nasa.gov>
2022-09-14 15:57:21 -0700

    add DOY time label to cursor header

--- src/components/timeline/TimelineCursors.svelte
+++ src/components/timeline/TimelineCursors.svelte
@@ -1,13 +1,12 @@
-<svelte:options immutable={true} />
-
 <script lang="ts">
   import type { ScaleTime } from 'd3-scale';
   import { select } from 'd3-selection';
-  // import { getDoyTime } from '../../utilities/time';
+  import { getDoyTime } from '../../utilities/time';
 
   export let mouseOver: MouseOver;
   export let xScaleView: ScaleTime<number, number> | null = null;
   export let marginLeft: number = 50;
+  export let drawWidth: number = 0;
 
   $: onMouseOver(mouseOver);
 
@@ -16,13 +15,21 @@
   function onMouseOver(event: MouseOver | undefined) {
     if (event && xScaleView) {
       const { offsetX } = event.e;
-      // const unixEpochTime = xScaleView.invert(offsetX).getTime();
-      // const doyTime = getDoyTime(new Date(unixEpochTime));
+      const unixEpochTime = xScaleView.invert(offsetX).getTime();
+      const doyTime = getDoyTime(new Date(unixEpochTime));
       cancelAnimationFrame(raf);
 
-      if (offsetX >= 0) {
+      if (offsetX >= 0 && offsetX <= drawWidth) {
         raf = window.requestAnimationFrame(() => {
           const cursorDiv = getCursor();
+          const cursorLabel = cursorDiv.select('.timeline-cursor-label').node() as HTMLElement;
+          cursorLabel.textContent = doyTime;
+          const cursorLabelWidth = cursorLabel.getBoundingClientRect().width;
+          if (offsetX + cursorLabelWidth + 10 > drawWidth) {
+            cursorLabel.style.left = 'calc(-100% - 10px)';
+          } else {
+            cursorLabel.style.left = '10px';
+          }
           cursorDiv.style('opacity', 1.0).style('transform', `translateX(${offsetX + marginLeft - 1}px)`);
         });
       } else {
@@ -46,7 +53,10 @@
 </script>
 
 <div class="timeline-cursor-container">
-  <div class="timeline-cursor" />
+  <div class="timeline-cursor-header" />
+  <div class="timeline-cursor">
+    <div class="timeline-cursor-label" />
+  </div>
   <slot />
 </div>
 
@@ -57,15 +67,49 @@
     width: 100%;
   }
 
+  .timeline-cursor-header {
+    height: 1rem;
+    position: relative;
+  }
+
   .timeline-cursor {
-    background-color: var(--st-gray-50);
-    height: 100%;
+    height: calc(100% + 0.5rem);
     left: 0;
     opacity: 0;
     pointer-events: none;
     position: absolute;
-    top: 0;
+    top: 0.5rem;
     transform: translateX(0);
+  }
+
+  .timeline-cursor::before {
+    background-color: var(--st-gray-50);
+    border-radius: 100%;
+    content: '';
+    display: block;
+    height: 9px;
+    left: -4px;
+    position: absolute;
+    top: -0.5rem;
+    width: 9px;
+  }
+
+  .timeline-cursor::after {
+    background-color: var(--st-gray-50);
+    content: '';
+    display: block;
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: -0.5rem;
     width: 1px;
+  }
+
+  .timeline-cursor-label {
+    font-size: 10px;
+    left: 10px;
+    position: relative;
+    top: -11px;
+    white-space: nowrap;
   }
 </style>
