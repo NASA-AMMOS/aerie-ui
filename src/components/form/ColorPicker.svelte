381582b27d3576d92b06e339ac5ecfb84462b2eb
Aaron Plave <aaron.plave@jpl.nasa.gov>
2023-03-06 16:17:34 -0800

    Color preset picker

--- src/components/form/ColorPicker.svelte
+++ src/components/form/ColorPicker.svelte
@@ -2,18 +2,23 @@
 
 <script lang="ts">
   import { classNames } from '../../utilities/generic';
-
+  import { tooltip } from '../../utilities/tooltip';
   export let className: string = '';
   export let name: string = '';
+  export let tooltipText: string = '';
   export let value: string = '';
-
   $: inputClasses = classNames('color-picker', {
     [className]: !!className,
   });
 </script>
 
-<div class={inputClasses}>
+<div class={inputClasses} use:tooltip={{ content: tooltipText, placement: 'top' }}>
   <input {value} class="color-picker-input" on:input on:change type="color" {name} />
+  {#if !value}
+    <div class="color-wheel">
+      <div class="color-wheel-hue" />
+    </div>
+  {/if}
 </div>
 
 <style>
@@ -21,7 +26,6 @@
     display: flex;
     position: relative;
   }
-
   .color-picker:after {
     border: 1px solid rgb(0 0 0 / 25%);
     border-radius: 4px;
@@ -32,12 +36,11 @@
     position: absolute;
     top: 0;
     width: 100%;
+    z-index: 1;
   }
-
   .color-picker:hover:after {
     border: 1px solid rgb(0 0 0 / 80%);
   }
-
   .color-picker-input {
     -webkit-appearance: none;
     -moz-appearance: none;
@@ -52,20 +55,47 @@
     position: relative;
     width: 24px;
   }
-
   .color-picker-input::-webkit-color-swatch {
     border: none;
     border-radius: 4px;
   }
-
   .color-picker-input::-moz-color-swatch {
     border: none;
     border-radius: 4px;
   }
-
   .color-picker-input::-webkit-color-swatch-wrapper {
     border: none;
     border-radius: 4px;
     padding: 0;
+  }
+  .color-wheel {
+    height: 24px;
+    pointer-events: none;
+    position: absolute;
+    width: 24px;
+    z-index: 1;
+  }
+  .color-wheel-hue {
+    background: radial-gradient(50% 50% at 50% 50%, #ffffff 0%, transparent 100%),
+      conic-gradient(
+        from 0deg at 50% 50%,
+        #ff0000 0deg,
+        #ffa800 47.73deg,
+        #ffff00 79.56deg,
+        #00ff00 121.33deg,
+        #00ffff 180.99deg,
+        #0000ff 238.67deg,
+        #ff00ff 294.36deg,
+        #ff0000 360deg
+      ),
+      #c4c4c4;
+    border-radius: 4px;
+    box-sizing: border-box;
+    height: 100%;
+    left: 0px;
+    position: absolute;
+    top: 0px;
+    transform: rotateZ(90deg);
+    width: 100%;
   }
 </style>
---
+++ src/components/form/ColorPresetsPicker.svelte
@@ -0,0 +1,112 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { getTarget } from '../../utilities/generic';
+  import { tooltip } from '../../utilities/tooltip';
+  import Menu from '../menus/Menu.svelte';
+  import ColorPicker from './ColorPicker.svelte';
+  export let value: string = '';
+  export let tooltipText: string = 'Color';
+  export let presetColors: string[] = ['#ef8b8c', '#febd85'];
+  let pickerMenu: Menu;
+  $: colorIsCustom = presetColors.indexOf(value) < 0;
+  const dispatch = createEventDispatcher();
+  function onInput(value: string) {
+    dispatch('input', { value });
+  }
+  function onColorPickerInput(event: Event) {
+    const { value } = getTarget(event);
+    onInput(value as string);
+  }
+</script>
+
+<button
+  class="st-button color-preset-picker color"
+  use:tooltip={{ content: tooltipText, placement: 'top' }}
+  style={`position: relative; background: ${value}`}
+  on:click|stopPropagation={() => pickerMenu.toggle()}
+>
+  <Menu bind:this={pickerMenu} hideAfterClick={false} placement="bottom-end">
+    <div class="colors">
+      {#each presetColors as color}
+        <button
+          class="st-button tertiary color"
+          on:click={() => onInput(color)}
+          style="background:{color}"
+          class:active={color === value}
+        />
+      {/each}
+      <ColorPicker
+        tooltipText="Custom Color"
+        value={colorIsCustom ? value : ''}
+        name="color-presets-picker"
+        on:input={onColorPickerInput}
+        className={colorIsCustom ? 'active' : ''}
+      />
+    </div>
+  </Menu>
+</button>
+
+<style>
+  .color-preset-picker :global(.menu) {
+    pointer-events: none;
+  }
+  .colors {
+    cursor: auto;
+    display: grid;
+    grid-gap: 8px;
+    grid-template-columns: repeat(4, 1fr);
+    padding: 8px;
+    pointer-events: auto;
+  }
+  .color {
+    border: 1px solid rgb(0, 0, 0, 24%);
+    border-radius: 4px;
+    height: 24px;
+    padding: 0;
+    position: relative;
+    width: 24px;
+  }
+  .color:hover:not(.active) {
+    border: 1px solid rgb(0 0 0 / 80%);
+  }
+  .color.active:before {
+    border: 2px solid #2f80ed;
+    border-radius: 6px;
+    content: ' ';
+    height: 30px;
+    left: -4px;
+    position: absolute;
+    top: -4px;
+    width: 30px;
+    z-index: 1;
+  }
+  .color-preset-picker :global(.color-picker.active):before {
+    background: linear-gradient(white, white) padding-box,
+      conic-gradient(
+          from 0deg at 50% 50%,
+          #ff0000 0deg,
+          #ffa800 47.73deg,
+          #ffff00 79.56deg,
+          #00ff00 121.33deg,
+          #00ffff 180.99deg,
+          #0000ff 238.67deg,
+          #ff00ff 294.36deg,
+          #ff0000 360deg
+        )
+        border-box;
+    border: 2px solid transparent;
+    border-radius: 6px;
+    content: ' ';
+    height: 30px;
+    left: -3px;
+    position: absolute;
+    top: -3px;
+    width: 30px;
+    z-index: 0;
+  }
+  .color-preset-picker :global(.color-picker) {
+    width: 24px;
+  }
+</style>
