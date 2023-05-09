1b2d8bc0b56316a0180edf0c731231630fa64c7e
Aaron Plave <aaron.plave@jpl.nasa.gov>
2023-05-09 16:52:43 -0700

    Add new metadata fields to ModelSchema

--- src/types/model.ts
+++ src/types/model.ts
@@ -5,13 +5,19 @@ export type Model = ModelSchema;
 export type ModelInsertInput = Pick<Model, 'jar_id' | 'mission' | 'name' | 'version'>;
 
 export type ModelSchema = {
+  created_at: string;
+  description: string;
   id: number;
   jar_id: number;
   mission: string;
   name: string;
+  owner: string;
   parameters: { parameters: ParametersMap };
   plans: { id: number }[];
   version: string;
 };
 
-export type ModelSlim = Pick<Model, 'id' | 'jar_id' | 'name' | 'plans' | 'version'>;
+export type ModelSlim = Pick<
+  Model,
+  'created_at' | 'description' | 'id' | 'jar_id' | 'name' | 'plans' | 'owner' | 'version'
+>;
