e9dee5db17b6c3375d5d326ab435fab3e8865736
Aaron Plave <aaron.plave@jpl.nasa.gov>
2023-05-10 11:53:16 -0700

    Tweak constraints page columns

--- src/stores/constraints.ts
+++ src/stores/constraints.ts
@@ -36,7 +36,7 @@ export const checkConstraintsStatus: Writable<Status | null> = writable(null);
 
 export const constraintViolationsResponse: Writable<ConstraintViolation[]> = writable([]);
 
-export const constraintsColumns: Writable<string> = writable('1fr 3px 2fr');
+export const constraintsColumns: Writable<string> = writable('1fr 3px 1fr');
 
 /* Derived. */
 
