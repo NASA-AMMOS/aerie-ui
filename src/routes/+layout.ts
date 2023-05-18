be0e39653d49c76619eb98b257dec5cebf7dffa3
Chris <camargo.cac@gmail.com>
2023-05-18 15:27:36 -0700

    Remove version store (#663)
    * Just query for version on the fly

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,8 @@
-import { base } from '$app/paths';
 import '../css/app.css';
-import { user as userStore, version as versionStore } from '../stores/app';
+import { user as userStore } from '../stores/app';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async ({ data, fetch }) => {
-  const versionResponse = await fetch(`${base}/version.json`);
-  const version = await versionResponse.json();
-
-  // Set version store.
-  versionStore.set(version);
-
-  // Set user store.
-  userStore.set(data?.user);
-
+export const load: LayoutLoad = async ({ data }) => {
+  userStore.set(data?.user); // TODO: Remove this!
   return { ...data };
 };
