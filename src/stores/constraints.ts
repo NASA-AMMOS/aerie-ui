7e89b963d8c23e658862421d67b2f5df26946f53
Aaron Plave <aaron.plave@jpl.nasa.gov>
2023-05-22 17:12:30 -0700

    Tweak constraints css grid columns

--- src/stores/constraints.ts
+++ src/stores/constraints.ts
@@ -36,7 +36,7 @@ export const checkConstraintsStatus: Writable<Status | null> = writable(null);
 
 export const constraintViolationsResponse: Writable<ConstraintViolation[]> = writable([]);
 
-export const constraintsColumns: Writable<string> = writable('1fr 3px 1fr');
+export const constraintsColumns: Writable<string> = writable('2fr 3px 1fr');
 export const constraintsFormColumns: Writable<string> = writable('1fr 3px 2fr');
 
 /* Derived. */
