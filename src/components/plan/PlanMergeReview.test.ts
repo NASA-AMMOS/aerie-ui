7fd4eafe751274525170f1eea6091d9f4afd9aa8
Aaron Plave <aaron.plave@jpl.nasa.gov>
2023-05-24 17:49:50 -0700

    Fixes

--- src/components/plan/PlanMergeReview.test.ts
+++ src/components/plan/PlanMergeReview.test.ts
@@ -34,6 +34,7 @@ const mockMergeRequest: PlanMergeRequestSchema = {
 const mockInitialPlan: Plan = {
   child_plans: [{ id: 2, name: 'Branch 1' }],
   collaborators: [{ collaborator: 'tester 2' }],
+  created_at: '2023-02-16T00:00:00',
   duration: '168:00:00',
   end_time_doy: '2023-054T00:00:00',
   id: 1,
@@ -53,13 +54,15 @@ const mockInitialPlan: Plan = {
   },
   model_id: 1,
   name: 'Demo Plan',
-  owner: 'tester 1',
+  owner: 'spacecaptain',
   parent_plan: null,
   revision: 3,
   scheduling_specifications: [{ id: 1 }],
   simulations: [{ simulation_datasets: [{ id: 1 }] }],
   start_time: '2023-02-16T00:00:00',
   start_time_doy: '2023-047T00:00:00',
+  updated_at: '2023-02-16T00:00:00',
+  updated_by: 'redshirt',
 };
 
 const user: User = { allowedRoles: ['admin'], defaultRole: 'admin', id: 'foo', permissibleQueries: {}, token: '' };
