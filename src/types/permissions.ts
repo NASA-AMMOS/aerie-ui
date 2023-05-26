ba13a7ff36966d2fb5b9a63f9b9d913dedf30cb0
bduran <bryan.duran@jpl.nasa.gov>
2023-05-25 18:37:14 -0700

    documentation

--- src/types/permissions.ts
+++ src/types/permissions.ts
@@ -1,7 +1,9 @@
 export type PermissibleQueryResponse = {
+  // mutationType is for the queries that directly mutate the db
   mutationType: {
     fields: PermissibleQuery[];
   };
+  // queryType is for the queries that just read from the db
   queryType: {
     fields: PermissibleQuery[];
   };
