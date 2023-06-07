317af291ab429743d50de145fadfe487d72263dd
camargo <camargo.cac@gmail.com>
2023-06-07 12:11:07 -0700

    Add login page env var flag
    * Add PUBLIC_LOGIN_PAGE env var
    * Allows for enabling or disabling the UI login page
    * Update hooks and routes to read env var

--- src/routes/expansion/runs/+page.ts
+++ src/routes/expansion/runs/+page.ts
@@ -1,15 +1,18 @@
 import { base } from '$app/paths';
+import { env } from '$env/dynamic/public';
 import { redirect } from '@sveltejs/kit';
 import effects from '../../../utilities/effects';
+import { hasNoAuthorization } from '../../../utilities/permissions';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ parent }) => {
-  const { user } = await parent();
+  const { permissibleQueries, user } = await parent();
 
-  if (!user) {
+  if (env.PUBLIC_LOGIN_PAGE === 'enabled' && (!user || hasNoAuthorization(permissibleQueries))) {
     throw redirect(302, `${base}/login`);
   }
 
   const expansionRuns = await effects.getExpansionRuns();
+
   return { expansionRuns };
 };
