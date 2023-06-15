da93ea20ef194282dfd6adc4c69d317e5ed68263
bduran <bryan.duran@jpl.nasa.gov>
2023-06-15 15:20:35 -0700

    handle the case for when user is null when checking for authorization

---
+++ src/utilities/permissions.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from 'vitest';
+import { hasNoAuthorization } from './permissions';
+
+describe('hasNoAuthorization', () => {
+  test('Should return whether or not the user has authorization', () => {
+    expect(hasNoAuthorization(null)).toEqual(true);
+    expect(
+      hasNoAuthorization({
+        allowedRoles: ['admin', 'user'],
+        defaultRole: 'user',
+        id: 'foo',
+        permissibleQueries: {},
+        token: '',
+      }),
+    ).toEqual(true);
+
+    expect(
+      hasNoAuthorization({
+        allowedRoles: ['admin', 'user'],
+        defaultRole: 'user',
+        id: 'foo',
+        permissibleQueries: {
+          constraint: true,
+        },
+        token: '',
+      }),
+    ).toEqual(false);
+  });
+});
