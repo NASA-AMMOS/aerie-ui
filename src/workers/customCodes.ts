22f9700b7c0188cd4fa3861bd03017f3e69f3b78
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2023-08-23 15:30:15 -1000

    Added custom code enum which can be used to filter TypeScript diagnotics.
    For example I am using them to filter and find unbalanced relative and epoch warnings to create a quick action.

--- src/workers/customCodes.ts
+++ src/workers/customCodes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable sort-keys */
 /**
  * Represents an error code with an ID and a message.
  */
@@ -10,12 +11,28 @@ export type ErrorCode = {
  * Contains custom error code definitions along with their error messages.
  */
 export const CustomErrorCodes = {
+  Type: {
+    INVALID_ABSOLUTE: -1,
+    INVALID_ARGUMENT: -8,
+    INVALID_ENUM: -6,
+    INVALID_EPOCH: -2,
+    INVALID_INT: -4,
+    INVALID_NUMBER: -10,
+    INVALID_RANGE: -7,
+    INVALID_RELATIVE: -11,
+    INVALID_UNSIGN_INTEGER: -5,
+    MAX_ABSOLUTE: -12,
+    MAX_EPOCH: -13,
+    MAX_RELATIVE: -14,
+    UNBALANCED: -9,
+    UNCAUGHT_ARG: -3,
+  },
   /**
    * InvalidAbsoluteTime error code and message.
    */
   InvalidAbsoluteTime: (): ErrorCode => {
     return {
-      id: -1,
+      id: CustomErrorCodes.Type.INVALID_ABSOLUTE,
       message: `Time Error: Incorrectly formatted absolute time string.
 Received : Malformed Absolute time.
 Expected: YYYY-DOYThh:mm:ss[.sss]`,
@@ -27,7 +44,7 @@ Expected: YYYY-DOYThh:mm:ss[.sss]`,
    */
   InvalidArgumentCount: (argName: string, repeatArgCount: number, min: number, max: number): ErrorCode => {
     return {
-      id: -8,
+      id: CustomErrorCodes.Type.INVALID_ARGUMENT,
       message: `Argument Count Error: The provided arguments for '${argName}' is outside the valid range.
 Received: ${repeatArgCount} repeating arguments.
 Expected: Repetition within the range of [${min}, ${max}]]`,
@@ -39,7 +56,7 @@ Expected: Repetition within the range of [${min}, ${max}]]`,
    */
   InvalidEnum: (argName: string, argValue: string, expectedValue: string): ErrorCode => {
     return {
-      id: -6,
+      id: CustomErrorCodes.Type.INVALID_ENUM,
       message: `ENUM Error: '${argName}' Unexpected ENUM argument.
 Received: ${argValue}
 Expected: ${expectedValue}`,
@@ -51,7 +68,7 @@ Expected: ${expectedValue}`,
    */
   InvalidEpochTime: (): ErrorCode => {
     return {
-      id: -2,
+      id: CustomErrorCodes.Type.INVALID_EPOCH,
       message: `Time Error: Incorrectly formatted duration string.
 Received: A malformed duration.
 Expected: [+/-]hh:mm:ss[.sss] or [+/-]DDDThh:mm:ss[.sss]`,
@@ -63,7 +80,7 @@ Expected: [+/-]hh:mm:ss[.sss] or [+/-]DDDThh:mm:ss[.sss]`,
    */
   InvalidInteger: (argName: string, argValue: string): ErrorCode => {
     return {
-      id: -4,
+      id: CustomErrorCodes.Type.INVALID_INT,
       message: `Number Error: '${argName}' is not a integer.
 Received: ${argValue}
 Expected: An integer.`,
@@ -75,7 +92,7 @@ Expected: An integer.`,
    */
   InvalidNumber: (argName: string, argValue: string): ErrorCode => {
     return {
-      id: -4,
+      id: CustomErrorCodes.Type.INVALID_NUMBER,
       message: `Number Error: '${argName}' Invalid Argument Type
 Received: ${argValue}
 Expected: A valid number.`,
@@ -87,7 +104,7 @@ Expected: A valid number.`,
    */
   InvalidRange: (argName: string, argValue: number, min: number, max: number): ErrorCode => {
     return {
-      id: -7,
+      id: CustomErrorCodes.Type.INVALID_RANGE,
       message: `Range Error: '${argName}' Out of Bounds.
 Received: ${argValue}
 Expected: A number within the range of [${min}, ${max}]].`,
@@ -99,7 +116,7 @@ Expected: A number within the range of [${min}, ${max}]].`,
    */
   InvalidRelativeTime: (): ErrorCode => {
     return {
-      id: -2,
+      id: CustomErrorCodes.Type.INVALID_RELATIVE,
       message: `Time Error: Incorrectly formatted duration string.
 Received: A malformed duration.
 Expected: hh:mm:ss[.sss]`,
@@ -111,7 +128,7 @@ Expected: hh:mm:ss[.sss]`,
    */
   InvalidUnsignedInteger: (argName: string, argValue: string): ErrorCode => {
     return {
-      id: -5,
+      id: CustomErrorCodes.Type.INVALID_UNSIGN_INTEGER,
       message: `Number Error: '${argName}' is not unsigned.
 Received: ${argValue}
 Expected: A signed value`,
@@ -123,7 +140,7 @@ Expected: A signed value`,
    */
   MaxAbsoluteTime: (balancedTime: string): ErrorCode => {
     return {
-      id: -1,
+      id: CustomErrorCodes.Type.MAX_ABSOLUTE,
       message: `Time Error: Maximum time reached
   Received: Balanced time - ${balancedTime}.
   Expected: ${balancedTime} <= 9999-365T23:59:59.999`,
@@ -135,7 +152,7 @@ Expected: A signed value`,
    */
   MaxEpochTime: (balancedTime: string): ErrorCode => {
     return {
-      id: -2,
+      id: CustomErrorCodes.Type.MAX_EPOCH,
       message: `Time Error: Maximum time reached.
 Received: Balanced time - ${balancedTime}.
 Expected: ${balancedTime} <= 365T23:59:59.999`,
@@ -147,7 +164,7 @@ Expected: ${balancedTime} <= 365T23:59:59.999`,
    */
   MaxRelativeTime: (balancedTime: string): ErrorCode => {
     return {
-      id: -2,
+      id: CustomErrorCodes.Type.MAX_RELATIVE,
       message: `Time Error: Maximum time reached.
   Received: Balanced time - ${balancedTime}.
   Expected: ${balancedTime} <= 23:59:59.999`,
@@ -158,15 +175,15 @@ Expected: ${balancedTime} <= 365T23:59:59.999`,
    */
   UnbalancedTime: (balancedTime: string): ErrorCode => {
     return {
-      id: -9,
+      id: CustomErrorCodes.Type.UNBALANCED,
       message: `Time Warning: Unbalanced time used.
-Suggestion: Change time to ${balancedTime}`,
+Suggestion: ${balancedTime}`,
     };
   },
   /**
    * UncaughtArgumentType error code and message.
    */
   UncaughtArgumentType: (): ErrorCode => {
-    return { id: -3, message: `Error: Command Argument Type Mismatch` };
+    return { id: CustomErrorCodes.Type.UNCAUGHT_ARG, message: `Error: Command Argument Type Mismatch` };
   },
 };
