109b591fb77fc9695e6f9c44073aa031fe149531
Aaron Plave <aaron.plave@jpl.nasa.gov>
2023-09-27 12:14:01 -0700

    Add tags to plan snapshots

--- src/components/modals/CreatePlanSnapshotModal.svelte
+++ src/components/modals/CreatePlanSnapshotModal.svelte
@@ -2,27 +2,35 @@
 
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
+  import { tags } from '../../stores/tags';
+  import type { User } from '../../types/app';
   import type { Plan } from '../../types/plan';
+  import type { Tag, TagsChangeEvent } from '../../types/tags';
+  import effects from '../../utilities/effects';
+  import TagsInput from '../ui/Tags/TagsInput.svelte';
   import Modal from './Modal.svelte';
   import ModalContent from './ModalContent.svelte';
   import ModalFooter from './ModalFooter.svelte';
   import ModalHeader from './ModalHeader.svelte';
 
-  export let height: number = 270;
-  export let width: number = 280;
+  export let height: number | string = 'unset';
+  export let width: number = 380;
   export let plan: Plan;
+  export let user: User | null = null;
 
   const dispatch = createEventDispatcher();
 
+  let createButtonDisabled: boolean = true;
   let snapshotName: string = `${plan.name} - Snapshot`;
   let snapshotDescription: string = '';
-  let createButtonDisabled: boolean = true;
+  let snapshotTags: Tag[] = [];
 
+  /* TODO tie this into featurePermissions? */
   $: createButtonDisabled = snapshotName === '';
 
   function create() {
     if (!createButtonDisabled) {
-      dispatch('create', { description: snapshotDescription, name: snapshotName, plan });
+      dispatch('create', { description: snapshotDescription, name: snapshotName, plan, tags: snapshotTags });
     }
   }
 
@@ -31,6 +39,21 @@
     if (key === 'Enter') {
       event.preventDefault();
       create();
+    }
+  }
+
+  async function onTagsInputChange(event: TagsChangeEvent) {
+    const {
+      detail: { tag, type },
+    } = event;
+    if (type === 'remove') {
+      snapshotTags = snapshotTags.filter(t => t.name !== tag.name);
+    } else if (type === 'create' || type === 'select') {
+      let tagsToAdd: Tag[] = [tag];
+      if (type === 'create') {
+        tagsToAdd = (await effects.createTags([{ color: tag.color, name: tag.name }], user)) || [];
+      }
+      snapshotTags = snapshotTags.concat(tagsToAdd);
     }
   }
 </script>
@@ -61,6 +84,10 @@
         class="st-input w-100"
         name="description"
       />
+    </fieldset>
+    <fieldset>
+      <label for="plan-duration">Tags</label>
+      <TagsInput options={$tags} selected={snapshotTags} on:change={onTagsInputChange} />
     </fieldset>
   </ModalContent>
   <ModalFooter>
