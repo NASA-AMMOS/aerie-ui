6b95b31b947dbf8bcddb46aa841de5c78f6a38aa
Aaron Plave <aaron.plave@jpl.nasa.gov>
2023-11-19 10:15:24 -0800

    Use compted tooltip params when updating tooltip props

--- src/utilities/tooltip.ts
+++ src/utilities/tooltip.ts
@@ -1,17 +1,34 @@
 import type { Plugin, Props } from 'tippy.js';
 import tippy from 'tippy.js';
 
-/**
- * @see https://dev.to/danawoodman/svelte-quick-tip-using-actions-to-integrate-with-javascript-libraries-tippy-tooltips-2m94
- */
-export function tooltip(node: Element, params: any = {}): any {
+function processParams(node: Element, params: any = {}): any {
   // Determine the title to show. We want to prefer
   // the custom content passed in first, then the
   // HTML title attribute then the aria-label
   // in that order.
+  const newParams = { ...params };
   const custom = params.content;
   const label = node.getAttribute('aria-label');
   const content = custom || label;
+
+  // Support keyboard shortcut param
+  const shortcut = params.shortcut;
+  const shortcutLabel = params.shortcutLabel;
+  if (shortcut || shortcutLabel) {
+    newParams.allowHTML = true;
+    const shortcutContent = shortcut
+      ? `
+      <span class="tooltip-shortcut-container">
+        ${shortcutLabel ? `<span class="tooltip-shortcut-label">${shortcutLabel}</span>` : ''}
+        <span class="tooltip-shortcut">${shortcut}</span>
+      </span>
+    `
+      : '';
+    newParams.content = `<div class="tooltip--with-shortcut">
+        <div>${content}</div>
+        ${shortcutContent}
+      </div>`;
+  }
 
   // Let's make sure the "aria-label" attribute
   // is set so our element is accessible:
@@ -28,12 +45,20 @@ export function tooltip(node: Element, params: any = {}): any {
     htmlNode.title = '';
   }
 
+  return newParams;
+}
+
+/**
+ * @see https://dev.to/danawoodman/svelte-quick-tip-using-actions-to-integrate-with-javascript-libraries-tippy-tooltips-2m94
+ */
+export function tooltip(node: Element, params: any = {}): any {
+  const processedParams = processParams(node, params);
+
   // Support any of the Tippy props by forwarding all "params":
   // https://atomiks.github.io/tippyjs/v6/all-props/
   const tip: any = tippy(node, {
-    content,
+    ...processedParams,
     plugins: [disabled],
-    ...params,
   });
 
   return {
@@ -41,7 +66,11 @@ export function tooltip(node: Element, params: any = {}): any {
     destroy: () => tip.destroy(),
 
     // If the props change, let's update the Tippy instance.
-    update: (newParams: any) => tip.setProps({ content, ...newParams }),
+    // TODO this does not take any of the constructor functionality into account
+    update: (newParams: any) => {
+      const processedParams = processParams(node, newParams);
+      tip.setProps({ ...processedParams });
+    },
   };
 }
 
