89279c48f1d5d6ff68ed7044f4c3a486b2fb6c36
(skovati) Luke <skovati@protonmail.com>
2023-12-12 15:53:02 -0800

    update tests for new login flow

--- src/utilities/login.test.ts
+++ src/utilities/login.test.ts
@@ -1,4 +1,3 @@
-import { env } from '$env/dynamic/public';
 import { afterEach, describe, expect, test, vi } from 'vitest';
 import { shouldRedirectToLogin } from './login';
 import { ADMIN_ROLE } from './permissions';
@@ -16,8 +15,6 @@ describe('login util functions', () => {
 
   describe('shouldRedirectToLogin', () => {
     test('Should determine if the route should redirect to the login page when login is enabled', () => {
-      vi.mocked(env).PUBLIC_LOGIN_PAGE = 'enabled';
-
       expect(shouldRedirectToLogin(null)).toEqual(true);
       expect(
         shouldRedirectToLogin({
@@ -30,37 +27,6 @@ describe('login util functions', () => {
           token: 'foo',
         }),
       ).toEqual(true);
-
-      expect(
-        shouldRedirectToLogin({
-          activeRole: 'user',
-          allowedRoles: [ADMIN_ROLE, 'user'],
-          defaultRole: 'user',
-          id: 'foo',
-          permissibleQueries: {
-            constraints: true,
-          },
-          rolePermissions: {},
-          token: 'foo',
-        }),
-      ).toEqual(false);
-    });
-
-    test('Should not redirect if login is disabled', () => {
-      vi.mocked(env).PUBLIC_LOGIN_PAGE = 'disabled';
-
-      expect(shouldRedirectToLogin(null)).toEqual(false);
-      expect(
-        shouldRedirectToLogin({
-          activeRole: 'user',
-          allowedRoles: [ADMIN_ROLE, 'user'],
-          defaultRole: 'user',
-          id: 'foo',
-          permissibleQueries: {},
-          rolePermissions: {},
-          token: 'foo',
-        }),
-      ).toEqual(false);
 
       expect(
         shouldRedirectToLogin({
