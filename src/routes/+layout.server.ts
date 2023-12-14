9ad13a53a028a75fbbde309fb6c40e2d612d33b4
(skovati) Luke <skovati@protonmail.com>
2023-12-14 15:40:59 -0800

    throw redirect instead of returning
    prevents `user` being typed as possibly undefined, which svelte-check didn't like

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
   if (!url.pathname.includes('login') && shouldRedirectToLogin(locals.user)) {
-    return redirect(302, base);
+    throw redirect(302, base);
   }
   return { ...locals };
 };
