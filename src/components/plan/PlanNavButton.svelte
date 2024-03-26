73bff202c37b1ca5a206c5c694e133cd0e03b7c8
Lyle Klyne <lyle.klyne@gmail.com>
2024-03-26 12:08:51 -0700

    add 500ms nav menu delay and treat as singletons

--- src/components/plan/PlanNavButton.svelte
+++ src/components/plan/PlanNavButton.svelte
@@ -1,3 +1,9 @@
+<script context="module">
+  import { writable } from 'svelte/store';
+
+  export const isNavHovered = writable(false);
+</script>
+
 <script lang="ts">
   import PlayIcon from '@nasa-jpl/stellar/icons/play.svg?component';
   import type { Status } from '../../enums/status';
@@ -23,14 +29,42 @@
   export let title: string;
 
   let menu: Menu;
+
+  // Add delay functions to handle the navbar button hover
+  let showTimeout: number | undefined;
+  let hideTimeout: number | undefined;
+
+  function delayedShow() {
+    // Clear any existing hide timeout to prevent hiding if we're showing again
+    if (hideTimeout !== undefined) clearTimeout(hideTimeout);
+
+    // If no other nav button is currently hovered, apply the delay
+    if (!$isNavHovered) {
+      showTimeout = window.setTimeout(() => {
+        console.log('delayedShow');
+        menu.show();
+        isNavHovered.set(true);
+      }, 500); // Show delay in milliseconds
+    } else {
+      // If another nav button is already hovered, show the menu immediately
+      menu.show();
+    }
+  }
+
+  function delayedHide() {
+    if (showTimeout !== undefined) clearTimeout(showTimeout);
+
+    hideTimeout = window.setTimeout(() => {
+      // Check if the mouse is not over any nav button
+      if (!document.querySelector('.nav-button:hover')) {
+        menu.hide();
+        isNavHovered.set(false);
+      }
+    }, 500); // Hide delay in milliseconds
+  }
 </script>
 
-<div
-  class="nav-button st-typography-medium"
-  role="none"
-  on:mouseenter={() => menu.show()}
-  on:mouseleave={() => menu.hide()}
->
+<div class="nav-button st-typography-medium" role="none" on:mouseenter={delayedShow} on:mouseleave={delayedHide}>
   <div class="nav-button-icon-container">
     <slot />
     <span class="nav-button-status">
