9dbba3b9654968bc3f842d53e01ed577ba15d948
bduran <bryan.duran@jpl.nasa.gov>
2024-03-28 12:08:19 -0700

    disable button if tags were somehow updated when they don't have permission

--- src/components/ui/Association/AssociationForm.svelte
+++ src/components/ui/Association/AssociationForm.svelte
@@ -105,7 +105,7 @@
   let owner: UserId = initialOwner ?? user?.id ?? null;
   let isPublic: boolean = initialPublic;
   let isDefinitionModified: boolean = false;
-  let isDefinitionDataModified: boolean = false;
+  let isDefinitionTagsModified: boolean = false;
   let isMetadataModified: boolean = false;
   let referenceModelId: number | null = initialReferenceModelId;
   let saveButtonEnabled: boolean = false;
@@ -133,7 +133,7 @@
     },
   );
   $: isDefinitionModified = diffDefinition({ definition: initialDefinitionCode }, { definition: definitionCode });
-  $: isDefinitionDataModified = diffTags(initialDefinitionTags || [], definitionTags);
+  $: isDefinitionTagsModified = diffTags(initialDefinitionTags || [], definitionTags);
   $: hasUpdateDefinitionPermission = hasWriteDefinitionTagsPermission || isDefinitionModified;
   $: pageTitle = mode === 'edit' ? 's' : 'New ';
   $: pageSubtitle = mode === 'edit' ? initialName : '';
@@ -143,11 +143,11 @@
     owner !== '' &&
     definitionCode !== '' &&
     name !== '' &&
-    (isMetadataModified || isDefinitionDataModified || isDefinitionModified);
+    (isMetadataModified || (isDefinitionTagsModified && hasUpdateDefinitionPermission) || isDefinitionModified);
   $: saveButtonClass = saveButtonEnabled ? 'primary' : 'secondary';
   $: if (mode === 'edit' && (isMetadataModified || isDefinitionModified)) {
     saveButtonText = 'Saved';
-    if ((isMetadataModified || isDefinitionDataModified) && !isDefinitionModified) {
+    if ((isMetadataModified || isDefinitionTagsModified) && !isDefinitionModified) {
       saveButtonText = 'Save';
     } else if (isDefinitionModified) {
       saveButtonText = 'Save as new version';
@@ -285,7 +285,7 @@
       if (isMetadataModified) {
         await saveMetadata();
       }
-      if (isDefinitionDataModified && !isDefinitionModified) {
+      if (isDefinitionTagsModified && !isDefinitionModified) {
         await saveDefinitionRevisionTags();
       } else if (isDefinitionModified) {
         await createNewDefinition();
