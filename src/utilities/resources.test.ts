fbcdbb5fd192f933620b5de05dff9a59b596a32f
Aaron Plave <aaron.plave@jpl.nasa.gov>
2024-03-29 14:06:19 -0700

    Additional test coverage

--- src/utilities/resources.test.ts
+++ src/utilities/resources.test.ts
@@ -41,6 +41,39 @@ describe('sampleProfiles', () => {
           type: 'real',
         },
       },
+      {
+        dataset_id: 1,
+        duration: '384:00:00',
+        id: 2,
+        name: '/simple_data/activities_executed',
+        profile_segments: [
+          {
+            dataset_id: 1,
+            dynamics: 0,
+            is_gap: false,
+            profile_id: 2,
+            start_offset: '00:00:00',
+          },
+          {
+            dataset_id: 1,
+            dynamics: 2,
+            is_gap: false,
+            profile_id: 2,
+            start_offset: '2 days 19:40:54.345',
+          },
+          {
+            dataset_id: 1,
+            dynamics: 4,
+            is_gap: false,
+            profile_id: 2,
+            start_offset: '4 days 03:10:21.295',
+          },
+        ],
+        type: {
+          schema: { type: 'int' },
+          type: 'discrete',
+        },
+      },
     ];
 
     const resources: Resource[] = sampleProfiles(profiles, '2022-09-01T00:00:00+00:00');
@@ -59,6 +92,18 @@ describe('sampleProfiles', () => {
           { is_gap: false, x: 1662347421295, y: 566834.75 },
           { is_gap: false, x: 1662347421295, y: 566834.75 },
           { is_gap: false, x: 1663372800000, y: 566834.75 },
+        ],
+      },
+      {
+        name: '/simple_data/activities_executed',
+        schema: { type: 'int' },
+        values: [
+          { is_gap: false, x: 1661990400000, y: 0 },
+          { is_gap: false, x: 1662234054345, y: 0 },
+          { is_gap: false, x: 1662234054345, y: 2 },
+          { is_gap: false, x: 1662347421295, y: 2 },
+          { is_gap: false, x: 1662347421295, y: 4 },
+          { is_gap: false, x: 1663372800000, y: 4 },
         ],
       },
     ];
