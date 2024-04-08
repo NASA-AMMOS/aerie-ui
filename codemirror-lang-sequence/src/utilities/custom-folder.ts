02bd185d5fe93dc19adce6c22590ab50656861a1
Chet Joswig <cjoswig@yahoo.com>
2024-04-07 21:59:10 -0700

    fix newline on folded command

--- codemirror-lang-sequence/src/utilities/custom-folder.ts
+++ codemirror-lang-sequence/src/utilities/custom-folder.ts
@@ -1,16 +1,17 @@
+import type { EditorState } from '@codemirror/state';
 import type { SyntaxNode } from '@lezer/common';
 
-export function customFoldInside(node: SyntaxNode): { from: number; to: number } | null {
+export function customFoldInside(node: SyntaxNode, state: EditorState): { from: number; to: number } | null {
   if (node.name === 'Command') {
-    return foldCommand(node);
+    return foldCommand(node, state);
   }
   return null;
 }
 
-function foldCommand(node: SyntaxNode): { from: number; to: number } | null {
+function foldCommand(node: SyntaxNode, state: EditorState): { from: number; to: number } | null {
   const stemNode = node.getChild('Stem');
   const argsNodes = node.getChildren('Args');
-  const commetNode = node.getChild('LineComment');
+  const commentNode = node.getChild('LineComment');
   const metadataNode = node.getChildren('Metadata');
   const modelNodes = node.getChildren('Models');
 
@@ -18,11 +19,15 @@ function foldCommand(node: SyntaxNode): { from: number; to: number } | null {
     return null;
   }
 
-  const from = calculateStartAndEnd([stemNode, ...argsNodes, commetNode]).to;
-  const to = calculateStartAndEnd([...metadataNode, ...modelNodes]).to;
+  const from = calculateStartAndEnd([stemNode, ...argsNodes, commentNode]).to;
+  const nonCommandTo = calculateStartAndEnd([...metadataNode, ...modelNodes]).to;
+  const text = state.sliceDoc(from, nonCommandTo);
+  // Exclude the last new line so commands remain on different lines
+  const to = from + text.lastIndexOf('\n');
   return { from, to };
 }
 
+// FIXME -- this looks like copy paste of getFromAndTo()
 function calculateStartAndEnd(nodes: (SyntaxNode | null)[]): { from: number; to: number } {
   return nodes.reduce(
     (acc, node) => {
