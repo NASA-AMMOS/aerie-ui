4ddf354bddc752cf1ab391b14d89c72c5100dee6
Ryan Goetz <Ryan.R.Goetz@jpl.nasa.gov>
2024-04-29 11:25:51 -1000

    Include seq_json when saving UserSequence

--- src/components/sequencing/SequenceForm.svelte
+++ src/components/sequencing/SequenceForm.svelte
@@ -118,8 +118,11 @@
     sequenceSeqJson = '';
   }
 
-  function onSequenceChange(event: CustomEvent) {
-    sequenceDefinition = event.detail;
+  function onSequenceChange(event: CustomEvent<{ seqJson: string; sequence: string }>) {
+    const { detail } = event;
+    const { seqJson, sequence } = detail;
+    sequenceDefinition = sequence;
+    sequenceSeqJson = seqJson;
   }
 
   function onDidChangeModelContent(event: CustomEvent<{ value: string }>) {
@@ -145,6 +148,7 @@
             definition: sequenceDefinition,
             name: sequenceName,
             parcel_id: sequenceParcelId,
+            seq_json: sequenceSeqJson,
           };
           const newSequenceId = await effects.createUserSequence(newSequence, user);
 
@@ -156,6 +160,7 @@
             definition: sequenceDefinition,
             name: sequenceName,
             parcel_id: sequenceParcelId,
+            seq_json: sequenceSeqJson,
           };
           const updated_at = await effects.updateUserSequence(sequenceId, updatedSequence, sequenceOwner, user);
           if (updated_at !== null) {
