bba382d3cda629d74a128962c7978f0b2d202dca
Jonathan Morton <jonathan.r.morton@nasa.gov>
2024-05-30 11:21:30 -0500

    Define more types for sources and events

--- src/types/external-event.ts
+++ src/types/external-event.ts
@@ -1,7 +1,13 @@
-export type ExternalEvent = ExternalEventSchema;
+import type { ExternalSource } from './external-source';
 
-export type ExternalEventInsertInput = Pick<ExternalEventSchema, 'id'>;
-
-export type ExternalEventSchema = {
+export type ExternalEvent = {
+  duration: string;
   id: number;
+  key: string;
+  properties: Record<string, any>;
+  source?: ExternalSource;
+  source_id: number;
+  start_time: string;
 };
+
+export type ExternalEventInsertInput = Pick<ExternalEvent, 'key' | 'start_time' | 'duration' | 'properties'>;
