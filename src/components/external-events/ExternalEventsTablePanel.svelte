63e062496c717b307d39cfc6df17a6f742bf3c4c
psubram3 <pranav.subramanian@nasa.gov>
2024-06-06 09:52:04 -0700

    create ExternalEventsTablePanel.svelte
    Prior to getting events on a timeline, we want clear visibility of events in a plan. This is an easy way to do so - a table!

---
+++ src/components/external-events/ExternalEventsTablePanel.svelte
@@ -0,0 +1,93 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import type { ValueGetterParams } from 'ag-grid-community';
+  import { externalEventsDB } from '../../stores/external-event';
+  import type { User } from '../../types/app';
+  import type { DataGridColumnDef } from '../../types/data-grid';
+  import type { ExternalSourceSlim } from '../../types/external-source';
+  import type { ViewGridSection } from '../../types/view';
+  import GridMenu from '../menus/GridMenu.svelte';
+  import SingleActionDataGrid from '../ui/DataGrid/SingleActionDataGrid.svelte';
+  import Panel from '../ui/Panel.svelte';
+
+  export let gridSection: ViewGridSection;
+  export let user: User | null;
+
+  const baseColumnDefs: DataGridColumnDef[] = [
+    {
+      field: 'id',
+      filter: 'number',
+      headerName: 'ID',
+      resizable: true,
+      sortable: true,
+    },
+    {
+      field: 'key',
+      filter: 'text',
+      headerName: 'Key',
+      resizable: true,
+      sortable: true,
+    },
+    {
+      field: 'event_type',
+      filter: 'text',
+      headerName: 'Event Type',
+      resizable: true,
+      sortable: true,
+    },
+    {
+      field: 'source_id',
+      filter: 'number',
+      headerName: 'Source ID',
+      resizable: true,
+      sortable: true,
+    },
+    {
+      field: 'start_time',
+      filter: 'text',
+      headerName: 'Start Time',
+      resizable: true,
+      sortable: true,
+      valueGetter: (params: ValueGetterParams<ExternalSourceSlim>) => {
+        if (params.data?.start_time) {
+          return new Date(params.data?.start_time).toISOString().slice(0, 19);
+        }
+      },
+    },
+    {
+      field: 'duration',
+      filter: 'text',
+      headerName: 'Duration',
+      resizable: true,
+      sortable: true,
+    },
+    // { // TODO: figure out a dropdown or something for this....
+    //   field: 'properties',
+    //   filter: 'record',
+    //   headerName: 'Properties',
+    //   resizable: true,
+    //   sortable: true,
+    // },
+  ];
+
+  let columnDefs = baseColumnDefs;
+</script>
+
+<Panel padBody={false}>
+  <svelte:fragment slot="header">
+    <GridMenu {gridSection} title="External Events Table" />
+  </svelte:fragment>
+
+  <svelte:fragment slot="body">
+    <SingleActionDataGrid
+      {columnDefs}
+      itemDisplayText="External Source"
+      items={$externalEventsDB}
+      {user}
+    />
+  </svelte:fragment>
+</Panel>
+
+<style>
+</style>
