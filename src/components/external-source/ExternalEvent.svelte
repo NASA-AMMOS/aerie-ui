1a84a4e6eb6ab40032025a90511497554f368349
psubram3 <pranav.subramanian@nasa.gov>
2024-06-06 09:55:58 -0700

    remove extraneous files

--- src/components/external-source/ExternalEvent.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  export let event: any;
-</script>
-
-<div>{event.event_type} @ {event.time}</div>
--- src/components/external-source/ExternalSource.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  export let source: any;
-</script>
-
-<div>This source contains {source.events.length} events.</div>
