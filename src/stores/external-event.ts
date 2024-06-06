d300212d38d20ef365358198bf320e472450c134
psubram3 <pranav.subramanian@nasa.gov>
2024-06-06 12:31:58 -0700

    filter external events by type

--- src/stores/external-event.ts
+++ src/stores/external-event.ts
@@ -1,3 +1,4 @@
+import { derived } from 'svelte/store';
 import type { ExternalEventDB } from "../types/external-event";
 import gql from '../utilities/gql';
 import { selectedPlanExternalSourceIds } from './external-source';
@@ -8,4 +9,9 @@ export const externalEventsDB = gqlSubscribable<ExternalEventDB[]>(
   { source_ids: selectedPlanExternalSourceIds },
   [],
   null,
+);
+
+export const externalEventTypes = derived(
+  externalEventsDB,
+  ($externalEventsDB) => $externalEventsDB.map(event => event.event_type).filter((val, index, arr) => arr.indexOf(val) === index) // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
 );
