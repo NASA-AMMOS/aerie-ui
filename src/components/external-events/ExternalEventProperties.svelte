b2a0396f6a66cfe2cfe2f760ee801900c88e2388
psubram3 <pranav.subramanian@nasa.gov>
2024-06-07 13:01:56 -0700

    change "Property" -> "ExternalEventProperty"

--- src/components/external-events/ExternalEventProperties.svelte
+++ src/components/external-events/ExternalEventProperties.svelte
@@ -1,13 +1,13 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-  import type { Property } from '../../types/property';
+  import type { ExternalEventProperty } from '../../types/external-event-property';
   import { compare } from '../../utilities/generic';
   import Highlight from '../ui/Highlight.svelte';
-  import PropertyBase from './PropertyBase.svelte';
-  import PropertyRec from './PropertyRec.svelte';
+  import ExternalEventPropertyBase from './ExternalEventPropertyBase.svelte';
+  import ExternalEventPropertyRec from './ExternalEventPropertyRec.svelte';
 
-  export let formProperties: Property[] = [];
+  export let formProperties: ExternalEventProperty[] = [];
   export let highlightKeysMap: Record<string, boolean> = {};
   
   $: sortedFormProperties = formProperties.sort((a, b) => compare(a.name, b.name));
@@ -17,12 +17,12 @@
   {#each sortedFormProperties as formProperty (formProperty.name)}
     <Highlight highlight={highlightKeysMap[formProperty.name]}>
         {#if typeof formProperty.value == 'string' || typeof formProperty.value == 'number'}
-          <PropertyBase
+          <ExternalEventPropertyBase
             formProperty={formProperty}
             highlightKeysMap={highlightKeysMap}
           />
         {:else if typeof formProperty.value == 'object'}
-          <PropertyRec
+          <ExternalEventPropertyRec
             formProperty={formProperty}
             highlightKeysMap={highlightKeysMap}
           />
---
+++ src/types/external-event-property.ts
@@ -0,0 +1,5 @@
+
+export type ExternalEventProperty = {
+  name: string,
+  value: any
+};
