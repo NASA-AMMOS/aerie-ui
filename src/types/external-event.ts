7d0a7ea1712be2b0288cbfdc9e48413fb4a7fa0c
Joseph Volosin <joseph.p.volosin@nasa.gov>
2024-06-14 11:19:30 -0400

    Event types are creatable, updated ExternalEvent type

--- src/types/external-event.ts
+++ src/types/external-event.ts
@@ -4,7 +4,7 @@ export type ExternalEventId = number;
 
 export type ExternalEventDB = {
   duration: string;
-  event_type_id: number;
+  event_type: string;
   id: number;
   key: string;
   properties: Record<string, any>;
@@ -13,7 +13,11 @@ export type ExternalEventDB = {
   start_time: string;
 };
 
-export type ExternalEvent = ExternalEventDB & {
+export type ExternalEvent = Pick<
+  ExternalEventDB,
+  'duration' | 'id' | 'key' | 'properties' | 'source' | 'source_id' | 'start_time'
+> & {
+  event_type_id: number;
   durationMs: number;
   startMs: number;
 };
@@ -29,3 +33,8 @@ export type ExternalEventType = {
 
 // this doesn't do any actual filtering. extra keys in surplus of this are NOT checked.
 export type ExternalEventInsertInput = Pick<ExternalEventDB, 'key' | 'event_type_id' | 'start_time' | 'duration' | 'properties'>;
+
+export type ExternalEventTypeInsertInput = Pick<
+  ExternalEventType,
+  'name'
+>;
