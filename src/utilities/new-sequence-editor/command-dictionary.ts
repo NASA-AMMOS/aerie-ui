9c67fb4245e6ec1e60b70053a270afff71930cb8
Cody Hansen <codyhansen92@gmail.com>
2024-06-17 11:33:18 -1000

    Converted fswCommandArgDefault to use a switch

--- src/utilities/new-sequence-editor/command-dictionary.ts
+++ src/utilities/new-sequence-editor/command-dictionary.ts
@@ -20,121 +20,128 @@ import { logError } from './logger';
 export function fswCommandArgDefault(fswCommandArg: FswCommandArgument, enumMap: EnumMap): string {
   const { arg_type } = fswCommandArg;
 
-  if (arg_type === 'boolean') {
-    const booleanArg = fswCommandArg as FswCommandArgumentBoolean;
-    const { default_value } = booleanArg;
+  switch (arg_type) {
+    case 'boolean': {
+      const booleanArg = fswCommandArg as FswCommandArgumentBoolean;
+      const { default_value } = booleanArg;
 
-    if (default_value !== null) {
-      return default_value.toLowerCase();
-    } else {
-      return 'false';
+      if (default_value !== null) {
+        return default_value.toLowerCase();
+      } else {
+        return 'false';
+      }
     }
-  } else if (arg_type === 'enum') {
-    const enumArg = fswCommandArg as FswCommandArgumentEnum;
-    const enumSymbolValue =
-      enumMap[enumArg.enum_name]?.values[0]?.symbol ?? fswCommandArg.default_value ?? fswCommandArg.name;
-    return `"${enumSymbolValue}"` ?? 'UNKNOWN_ENUM';
-  } else if (arg_type === 'fill') {
-    return '""';
-  } else if (arg_type === 'fixed_string') {
-    return '""';
-  } else if (arg_type === 'float') {
-    const floatArg = fswCommandArg as FswCommandArgumentFloat;
-    const { default_value, range } = floatArg;
-
-    if (default_value !== null) {
-      return `${default_value}`;
-    } else if (range !== null) {
-      const { min } = range;
-      return `${min}`;
-    } else {
-      return '0.0';
+    case 'enum': {
+      const enumArg = fswCommandArg as FswCommandArgumentEnum;
+      const enumSymbolValue =
+        enumMap[enumArg.enum_name]?.values[0]?.symbol ?? fswCommandArg.default_value ?? fswCommandArg.name;
+      return `"${enumSymbolValue}"` ?? 'UNKNOWN_ENUM';
     }
-  } else if (arg_type === 'integer') {
-    const intArg = fswCommandArg as FswCommandArgumentInteger;
-    const { default_value, range } = intArg;
-
-    if (default_value !== null) {
-      return `${default_value}`;
-    } else if (range !== null) {
-      const { min } = range;
-      return `${min}`;
-    } else {
-      return '0';
-    }
-  } else if (arg_type === 'numeric') {
-    const numericArg = fswCommandArg as FswCommandArgumentNumeric;
-    const { default_value, range } = numericArg;
-
-    if (default_value !== null) {
-      return `${default_value}`;
-    } else if (range !== null) {
-      const { min } = range;
-      return `${min}`;
-    } else {
-      return '0.0';
-    }
-  } else if (arg_type === 'repeat') {
-    const repeatArg = fswCommandArg as FswCommandArgumentRepeat;
-    const { repeat } = repeatArg;
-
-    let defaultRepeatArg = '[';
-    let totalRepeatedArgs = 0;
-
-    if (repeat) {
-      const { min } = repeat;
-
-      do {
-        let repeatedArg = '';
-
-        for (let i = 0; i < repeat.arguments.length; ++i) {
-          const arg = repeat.arguments[i];
-          const argValue = fswCommandArgDefault(arg, enumMap);
-          repeatedArg += `${argValue}`;
-
-          if (i !== repeat.arguments.length - 1) {
-            repeatedArg += ' ';
-          }
-        }
-
-        defaultRepeatArg += repeatedArg;
-        ++totalRepeatedArgs;
-
-        // If we are going to add another repeated arg, make sure to add a comma.
-        if (min !== null && totalRepeatedArgs < min) {
-          defaultRepeatArg += ' ';
-        }
-      } while (min !== null && totalRepeatedArgs < min);
-    }
-
-    defaultRepeatArg += ']';
-
-    return defaultRepeatArg;
-  } else if (arg_type === 'time') {
-    return '0';
-  } else if (arg_type === 'unsigned') {
-    const numericArg = fswCommandArg as FswCommandArgumentUnsigned;
-    const { default_value, range } = numericArg;
-
-    if (default_value !== null) {
-      return `${default_value}`;
-    } else if (range !== null) {
-      const { min } = range;
-      return `${min}`;
-    } else {
-      return '0';
-    }
-  } else if (arg_type === 'var_string') {
-    const varStringArg = fswCommandArg as FswCommandArgumentVarString;
-    const { default_value } = varStringArg;
-
-    if (default_value !== null) {
-      return default_value;
-    } else {
+    case 'fill' || 'fixed_string':
       return '""';
+    case 'float': {
+      const floatArg = fswCommandArg as FswCommandArgumentFloat;
+      const { default_value, range } = floatArg;
+
+      if (default_value !== null) {
+        return `${default_value}`;
+      } else if (range !== null) {
+        const { min } = range;
+        return `${min}`;
+      } else {
+        return '0.0';
+      }
     }
-  } else {
-    return '';
+    case 'integer': {
+      const intArg = fswCommandArg as FswCommandArgumentInteger;
+      const { default_value, range } = intArg;
+
+      if (default_value !== null) {
+        return `${default_value}`;
+      } else if (range !== null) {
+        const { min } = range;
+        return `${min}`;
+      } else {
+        return '0';
+      }
+    }
+    case 'numeric': {
+      const numericArg = fswCommandArg as FswCommandArgumentNumeric;
+      const { default_value, range } = numericArg;
+
+      if (default_value !== null) {
+        return `${default_value}`;
+      } else if (range !== null) {
+        const { min } = range;
+        return `${min}`;
+      } else {
+        return '0.0';
+      }
+    }
+    case 'repeat': {
+      const repeatArg = fswCommandArg as FswCommandArgumentRepeat;
+      const { repeat } = repeatArg;
+
+      let defaultRepeatArg = '[';
+      let totalRepeatedArgs = 0;
+
+      if (repeat) {
+        const { min } = repeat;
+
+        do {
+          let repeatedArg = '';
+
+          for (let i = 0; i < repeat.arguments.length; ++i) {
+            const arg = repeat.arguments[i];
+            const argValue = fswCommandArgDefault(arg, enumMap);
+            repeatedArg += `${argValue}`;
+
+            if (i !== repeat.arguments.length - 1) {
+              repeatedArg += ' ';
+            }
+          }
+
+          defaultRepeatArg += repeatedArg;
+          ++totalRepeatedArgs;
+
+          // If we are going to add another repeated arg, make sure to add a comma.
+          if (min !== null && totalRepeatedArgs < min) {
+            defaultRepeatArg += ' ';
+          }
+        } while (min !== null && totalRepeatedArgs < min);
+      }
+
+      defaultRepeatArg += ']';
+
+      return defaultRepeatArg;
+    }
+    case 'time':
+      return '0';
+    case 'unsigned': {
+      const numericArg = fswCommandArg as FswCommandArgumentUnsigned;
+      const { default_value, range } = numericArg;
+
+      if (default_value !== null) {
+        return `${default_value}`;
+      } else if (range !== null) {
+        const { min } = range;
+        return `${min}`;
+      } else {
+        return '0';
+      }
+    }
+    case 'var_string': {
+      const varStringArg = fswCommandArg as FswCommandArgumentVarString;
+      const { default_value } = varStringArg;
+
+      if (default_value !== null) {
+        return default_value;
+      } else {
+        return '""';
+      }
+    }
+    default:
+      return '';
   }
 }
 
