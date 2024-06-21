7a3e8be77622710649f4a63a8bf3249c8e6b4a6e
Aaron Plave <aaron.plave@jpl.nasa.gov>
2024-06-21 10:26:26 -0700

    Use env variable instead of server route to control time plugin checking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,11 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import Nav from '../components/app/Nav.svelte';
-  import { plugins } from '../stores/plugins';
   import { modalBodyClickListener, modalBodyKeyListener } from '../utilities/modal';
-  import { loadPluginCode } from '../utilities/plugins';
-  import type { LayoutData } from './$types';
-
-  export let data: LayoutData;
-  let pluginsLoaded = data.timePluginPath ? false : true;
-
-  onMount(() => {
-    loadPlugins();
-  });
-
-  async function loadPlugins() {
-    if (data.timePluginPath) {
-      $plugins = await loadPluginCode(data.timePluginPath);
-    }
-    pluginsLoaded = true;
-  }
 </script>
 
 <svelte:body on:click={modalBodyClickListener} on:keydown={modalBodyKeyListener} />
 
-{#if pluginsLoaded}
-  <slot />
-{:else}
-  <div style="display: flex; flex-direction: column; height: 100%">
-    <Nav />
-    <div
-      class="st-typography-header"
-      style="align-items: center;display: flex; flex: 1; justify-content: center; width: 100%"
-    >
-      Loading plugins...
-    </div>
-  </div>
-{/if}
+<slot />
 
 <div id="svelte-modal" />
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,6 @@
 import '../css/app.css';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async ({ data, fetch }) => {
-  // TODO could do this only on plans and plan pages
-  const timePluginPath = await (await fetch('/plugins')).text();
-  return { ...data, timePluginPath };
+export const load: LayoutLoad = async ({ data }) => {
+  return { ...data };
 };
