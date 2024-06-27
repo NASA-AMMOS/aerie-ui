7527fc45333b99d02433805c516553a6c8a10c61
jpvolosi <joseph.p.volosin@nasa.gov>
2024-06-27 11:29:05 -0400

    Cleanup External Source tests & improve use of fixtures

--- e2e-tests/tests/external-sources.test.ts
+++ e2e-tests/tests/external-sources.test.ts
@@ -5,6 +5,15 @@ let page: Page;
 let context: BrowserContext;
 let externalSources: ExternalSources;
 
+/** Note: These tests should probably delete all External Sources & (optionally) re-upload the example source between iterations. Requires deletion to be implemented!
+test.afterEach(async () => {
+});
+*/
+
+test.beforeEach(async () => {
+  await externalSources.goto();  // Refresh page to reset the view
+});
+
 test.beforeAll(async ({ browser }) => {
   context = await browser.newContext();
   page = await context.newPage();
@@ -12,12 +21,7 @@ test.beforeAll(async ({ browser }) => {
   await externalSources.goto();
 });
 
-test.afterEach(async () => {
-  //await externalSources.deleteAllExternalSources(); TODO - this function is currently not implemented
-});
-
 test.afterAll(async () => {
-  //await externalSources.deleteAllExternalSources(); TODO - this function is currently not implemented
   await page.close();
   await context.close();
 });
@@ -34,39 +38,25 @@ test.describe.serial('External Sources', () => {
   });
 
   test('External event form should be shown when an event is selected', async () => {
-    await page.getByRole('button', { name: 'Filter External Sources' }).click();
-    await page.getByPlaceholder('Filter by Source Type').click();
-    await page.getByRole('button', { name: 'Select 1 external source type' }).click();
-    await page.getByRole('gridcell', { name: '1', exact: true }).nth(1).click();
-    await page.getByRole('gridcell', { name: 'DSNContact:79/MMS/MMS3:54:X/' }).click();
+    await externalSources.selectEvent();
     await expect(page.locator('#svelte div').filter({ hasText: 'Selected Event' }).nth(3)).toBeVisible();
     await expect(externalSources.inputFile).not.toBeVisible();
   });
 
   test('External source form should be shown when a source is selected', async () => {
-    await page.getByRole('button', { name: 'Filter External Sources' }).click();
-    await page.getByPlaceholder('Filter by Source Type').click();
-    await page.getByRole('button', { name: 'Select 1 external source type' }).click();
-    await page.getByRole('gridcell', { name: '1', exact: true }).nth(1).click();
+    await externalSources.selectSource();
     await expect(page.locator('.external-source-header-title-value')).toBeVisible();
     await expect(externalSources.externalEventSelectedForm).not.toBeVisible();
     await expect(externalSources.inputFile).not.toBeVisible();
   });
 
   test('External source deselection should be shown when an event is selected', async () => {
-    await page.getByRole('button', { name: 'Filter External Sources' }).click();
-    await page.getByPlaceholder('Filter by Source Type').click();
-    await page.getByRole('button', { name: 'Select 1 external source type' }).click();
-    await page.getByRole('gridcell', { name: '1', exact: true }).nth(1).click();
+    await externalSources.selectSource();
     await expect(page.getByLabel('Deselect source')).toBeVisible();
   });
 
   test('External event deselection should be shown when a source is selected', async () => {
-    await page.getByRole('button', { name: 'Filter External Sources' }).click();
-    await page.getByPlaceholder('Filter by Source Type').click();
-    await page.getByRole('button', { name: 'Select 1 external source type' }).click();
-    await page.getByRole('gridcell', { name: '1', exact: true }).nth(1).click();
-    await page.getByRole('gridcell', { name: 'DSNContact:79/MMS/MMS3:54:X/' }).click();
+    await externalSources.selectEvent();
     await expect(page.getByLabel('Deselect event')).toBeVisible();
   });
 
