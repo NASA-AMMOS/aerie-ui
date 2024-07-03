a9ef8d5e77fd10cdcce7df3a275637685d8ac538
psubram3 <pranav.subramanian@nasa.gov>
2024-07-03 14:27:45 -0700

    derivation group replacement part 1 (panels)

--- src/components/external-source/ExternalSourcesPanel.svelte
+++ src/components/external-source/ExternalSourcesPanel.svelte
@@ -1,10 +1,13 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
+  import { createExternalSourcePlanError, derivationGroups, externalSourceTypes, getEventSourceTypeName, selectedPlanDerivationGroupIds } from '../../stores/external-source';
+  import { plan } from '../../stores/plan';
   import { createExternalSourcePlanError, externalSourceWithTypeName, selectedPlanExternalSourceIds } from '../../stores/external-source';
   import type { User } from '../../types/app';
-  import type { ExternalSourceWithTypeName } from '../../types/external-source';
+  import type { DerivationGroup } from '../../types/external-source';
   import type { ViewGridSection } from '../../types/view';
+  import Collapse from '../Collapse.svelte';
   import CollapsibleListControls from '../CollapsibleListControls.svelte';
   import GridMenu from '../menus/GridMenu.svelte';
   import AlertError from '../ui/AlertError.svelte';
@@ -14,15 +17,28 @@
   export let gridSection: ViewGridSection;
   export let user: User | null;
 
-  // filter which sources are visible
+  // filter which derivation groups are visible
   let filterText: string = '';
-  let filteredExternalEvents: ExternalSourceWithTypeName[] = [];
-  $: filteredExternalEvents = $externalSourceWithTypeName
-    .filter(source => {
+  let mappedDerivationGroups: { [key: string]: DerivationGroup[] } = {};
+  let filteredDerivationGroups: DerivationGroup[] = [];
+  $: console.log("DERIVATION GROUPS: ", $derivationGroups)
+  $: filteredDerivationGroups = $derivationGroups
+    .filter(group => {
       const filterTextLowerCase = filterText.toLowerCase();
-      const includesName = source.key.toLocaleLowerCase().includes(filterTextLowerCase);
+      const includesName = group.name.toLocaleLowerCase().includes(filterTextLowerCase);
       return includesName;
     });
+  $: filteredDerivationGroups.forEach(group => {
+    const sourceType = getEventSourceTypeName(group.source_type_id, $externalSourceTypes)
+    if (sourceType) { // undefined is being very frustrating
+      if (mappedDerivationGroups[sourceType]) {// use string later for source type
+        mappedDerivationGroups[sourceType]?.push(group)
+      }
+      else {
+        mappedDerivationGroups[sourceType] = [group]
+      }
+    }
+  })
 </script>
 
 <Panel>
@@ -38,13 +54,29 @@
 
     <AlertError class="m-2" error={$createExternalSourcePlanError} />
 
-    {#if filteredExternalEvents.length}
-      {#each filteredExternalEvents as externalSource}
+    <!-- {#if filteredDerivationGroups.length}
+      {#each filteredDerivationGroups as derivationGroup}
         <ExternalSourcePanelEntry
-          enabled={$selectedPlanExternalSourceIds.includes(externalSource.id)}
-          externalSource={externalSource}
+          enabled={$selectedPlanExternalSourceIds.includes(derivationGroup.id)}
+          derivationGroup={derivationGroup}
+          plan_id={$plan?.id}
           user={user}
         />
+      {/each} -->
+    {#if filteredDerivationGroups.length}
+      {#each Object.keys(mappedDerivationGroups) as sourceType}
+        <Collapse title={sourceType.toString()} tooltipContent={sourceType.toString()} defaultExpanded={false}>
+          {#if mappedDerivationGroups[sourceType]}
+            {#each mappedDerivationGroups[sourceType] as group}
+              <ExternalSourcePanelEntry
+                enabled={$selectedPlanDerivationGroupIds.includes(group.id)}
+                derivationGroup={group}
+                plan_id={$plan?.id}
+                user={user}
+              />
+            {/each}
+          {/if}
+        </Collapse>
       {/each}
     {:else}
       <p>
