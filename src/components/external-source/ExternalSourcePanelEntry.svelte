6199844d706d81877e770da280ef5482cc4ef2a2
psubram3 <pranav.subramanian@nasa.gov>
2024-07-03 14:27:45 -0700

    derivation group replacement part 1 (panels)

--- src/components/external-source/ExternalSourcePanelEntry.svelte
+++ src/components/external-source/ExternalSourcePanelEntry.svelte
@@ -2,30 +2,36 @@
 
 <script lang="ts">
   import { externalEventTypes } from '../../stores/external-event';
+  import { externalSourceWithTypeName } from '../../stores/external-source';
   import { createExternalSourcePlanError } from '../../stores/external-source';
   import { plan } from '../../stores/plan';
   import type { User } from '../../types/app';
   import type { ExternalEventType } from '../../types/external-event';
-  import type { ExternalSourceWithTypeName } from '../../types/external-source';
+  import type { DerivationGroup, ExternalSourceWithResolvedNames } from '../../types/external-source';
   import effects from '../../utilities/effects';
   import Collapse from '../Collapse.svelte';
 
   export let enabled: boolean;
-  export let externalSource: ExternalSourceWithTypeName;
+  export let derivationGroup: DerivationGroup;
+  export let plan_id: number | undefined;
   export let user: User | null;
 
 
   let selectedSourceEventTypes: ExternalEventType[] | null = null;
+  let relevantSources: ExternalSourceWithResolvedNames[] = []
+  $: relevantSources = $externalSourceWithTypeName.filter(source => derivationGroup.name === source.derivation_group)
 
   function onEnable(event: Event) {
     if (enabled) {
-      effects.insertExternalSourceForPlan(externalSource.id, $plan, user);
+      // insert
+      effects.insertDerivationGroupForPlan(derivationGroup.id, $plan, user);
       if (createExternalSourcePlanError !== null) {
         enabled = false;  // Unselect button if there was an error
       }
     }
     else {
-      effects.deleteExternalSourceForPlan(externalSource.id, $plan, user);
+      // delete
+      effects.deleteDerivationGroupForPlan(derivationGroup.id, $plan, user);
       if (createExternalSourcePlanError !== null) {
         enabled = true;  // Reselect button if there was an error
       }
@@ -34,13 +40,13 @@
 
   async function getExternalEventTypes() {
     if (!selectedSourceEventTypes || selectedSourceEventTypes.length === 0) {
-      selectedSourceEventTypes = (await effects.getExternalEventTypesBySource([externalSource.id], $externalEventTypes, user))
+      selectedSourceEventTypes = (await effects.getExternalEventTypesBySource([derivationGroup.id], $externalEventTypes, user))
     }
   }
 </script>
 
 <div class="external-source-pairing">
-  <Collapse title={externalSource.key} tooltipContent={externalSource.key} defaultExpanded={false}>
+  <Collapse title={derivationGroup.name} tooltipContent={"Derivation group " + derivationGroup.name} defaultExpanded={false}>
     <span slot="left">
       <input
         type="checkbox"
@@ -51,38 +57,47 @@
       />
     </span>
 
-    <!-- Collapsible details -->
-    <p>
-      <strong>Key:</strong> {externalSource.key}
-    </p>
+    {#if relevantSources.length}
+      <Collapse title={"Contained Sources"} tooltipContent={"View sources in this group"} defaultExpanded={false}>
+        {#each relevantSources as source}
+          <!-- Collapsible details -->
+          <Collapse title={source.key} tooltipContent={source.key} defaultExpanded={false}>
+            <p>
+              <strong>Key:</strong> {source.key}
+            </p>
 
-    <p>
-      <strong>Source Type:</strong> {externalSource.source_type}
-    </p>
+            <p>
+              <strong>Source Type:</strong> {source.source_type}
+            </p>
 
-    <p>
-      <strong>Start Time:</strong> {externalSource.start_time}
-    </p>
+            <p>
+              <strong>Start Time:</strong> {source.start_time}
+            </p>
 
-    <p>
-      <strong>End Time:</strong> {externalSource.end_time}
-    </p>
+            <p>
+              <strong>End Time:</strong> {source.end_time}
+            </p>
 
-    <p>
-      <strong>ValidAt:</strong> {externalSource.valid_at}
-    </p>
-
-    <Collapse
-      className="anchor-collapse"
-      defaultExpanded={false}
-      title="Event Types"
-      tooltipContent="View Contained Event Types"
-      on:collapse={() => {getExternalEventTypes()}}
-    >
-      {#each selectedSourceEventTypes ? selectedSourceEventTypes : [{ id: -1, name: "None" }] as eventType}
-        <i>{eventType.name}</i>
-      {/each}
-    </Collapse>
+            <p>
+              <strong>ValidAt:</strong> {source.valid_at}
+            </p>
+          </Collapse>
+        {/each}
+      </Collapse>
+      <Collapse
+        className="anchor-collapse"
+        defaultExpanded={false}
+        title="Event Types"
+        tooltipContent="View Contained Event Types"
+        on:collapse={() => {getExternalEventTypes()}}
+      >
+        {#each selectedSourceEventTypes ? selectedSourceEventTypes : [{ id: -1, name: "None" }] as eventType}
+          <i>{eventType.name}</i>
+        {/each}
+      </Collapse>
+    {:else}
+      <p>No sources in this group.</p>
+    {/if}
   </Collapse>
 </div>
 
