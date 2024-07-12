3d5c0ec24e7bd7bda4ba558a3e7369a33fee49bc
psubram3 <pranav.subramanian@nasa.gov>
2024-07-12 15:10:01 -0700

    create card

---
+++ src/components/ui/UpdateCard.svelte
@@ -0,0 +1,144 @@
+<script lang="ts">
+  import LightningCharge from 'bootstrap-icons/icons/lightning-charge.svg?component';
+  import XIcon from 'bootstrap-icons/icons/x.svg?component';
+  import { createEventDispatcher } from 'svelte';
+  import type { ExternalSourceWithResolvedNames } from '../../types/external-source';
+  import { getTimeAgo } from '../../utilities/time';
+  import Collapse from '../Collapse.svelte';
+
+  export let newSources: ExternalSourceWithResolvedNames[] = [];
+
+  const dispatch = createEventDispatcher<{
+    dismiss: void;
+  }>();
+  let mappedSources: { [sourceType: string]: {[derivationGroup: string]: ExternalSourceWithResolvedNames[]} } = {};
+
+  $: newSources.forEach(source => {
+    let sourceType = source.source_type;
+    let derivationGroup = source.derivation_group;
+    if (sourceType && derivationGroup) {
+      if (mappedSources[sourceType] && mappedSources[sourceType][derivationGroup]) { 
+        mappedSources[sourceType][derivationGroup]?.push(source)
+      }
+      else if (mappedSources[sourceType] && !mappedSources[sourceType][derivationGroup]){
+        mappedSources[sourceType][derivationGroup] = [source]
+      }
+      else if (!mappedSources[sourceType]){
+        mappedSources[sourceType] = {[derivationGroup]: [source]}
+      }
+    }
+  })
+
+  function formatDateTime(date: string): string {
+    return new Date(date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+    });
+  }
+</script>
+
+<div class="card st-typography-label">
+  <div class="card--row card--title-row">
+    <div class="card--title t-typography-medium">
+      <LightningCharge/>{newSources.length} new files have been uploaded
+    </div>
+    <slot name="right">
+      <button
+          class="st-button icon fs-6"
+        >
+          <XIcon />
+        </button>
+    </slot>
+  </div>
+    <div class="card--row" style="padding-top: 5px">
+      <span class="st-typography-body">
+        New files matching source types in the current plan
+      </span>
+      <div class="card--metadata-row">
+    </div>
+  </div>
+  <div class="card--body st-typography-body">
+    {#each Object.keys(mappedSources) as sourceType}
+      <Collapse title={sourceType} tooltipContent={sourceType} defaultExpanded={true}>
+        {#each Object.keys(mappedSources[sourceType]) as derivationGroup}
+          <Collapse title={derivationGroup} tooltipContent={derivationGroup} defaultExpanded={true}>
+            {#each mappedSources[sourceType][derivationGroup] as source}
+              <div class="card--row">
+                <div class="card--metadata-row">
+                    <div class="card--date">
+                      <span class="st-typography-body">
+                        <p>{source.key}</p>
+                      </span>
+                      <span class="card--date-time-ago">
+                        <!-- {getTimeAgo(new Date(date), new Date(), Number.MAX_SAFE_INTEGER)} -->
+                        <p style:float="left" style:padding-top="0.1rem" style:padding-right="0.25rem" style:color="gray">
+                          {getTimeAgo(new Date(source.created_at), new Date(), Number.MAX_SAFE_INTEGER)}
+                        </p>
+                      </span>
+                    </div>
+                </div>
+              </div>
+            {/each}
+          </Collapse>
+        {/each}
+      </Collapse>
+    {/each}
+  </div>
+  <div style="padding-top:5px">
+    <button style="border: 0px; width: 100px; background: rgb(235, 235, 235)" on:click={() => dispatch("dismiss")}>Dismiss</button>
+  </div>
+  <slot />
+</div>
+
+<style>
+  .card {
+    background: var(--bg-color, rgba(245, 245, 245, 0.35));
+    border: 1px solid var(--border-color, rgba(249, 191, 29, 0.48));
+    border-radius: 4px;
+    color: var(--st-gray-70);
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    padding: 4px 14px 12px;
+    text-align: left;
+  }
+
+  .card--row {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    min-height: 24px;
+    width: 100%;
+  }
+
+  .card--title-row {
+    background: var(--title-bg-color, rgba(255, 245, 100, 0.24));
+    padding: 4px 0px;
+  }
+
+  .card--metadata-row {
+    display: flex;
+  }
+
+  .card--title {
+    color: var(--st-gray-80);
+    line-height: 24px;
+  }
+
+  .card--date {
+    display: flex;
+    gap: 20px;
+  }
+
+  .card--date-time-ago {
+    color: var(--st-gray-50);
+  }
+
+  .card--body {
+    color: var(--st-gray-70);
+    padding: 4px 0px;
+  }
+</style>
