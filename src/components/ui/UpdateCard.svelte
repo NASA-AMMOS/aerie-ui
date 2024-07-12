2f6bde4839c3684d23cce27b3672a73c73df77ac
psubram3 <pranav.subramanian@nasa.gov>
2024-07-12 15:53:30 -0700

    add formatting to card, persist storage across refreshes

--- src/components/ui/UpdateCard.svelte
+++ src/components/ui/UpdateCard.svelte
@@ -41,68 +41,66 @@
 </script>
 
 <div class="card st-typography-label">
-  <div class="card--row card--title-row">
-    <div class="card--title t-typography-medium">
-      <LightningCharge/>{newSources.length} new files have been uploaded
+  <div class="card--row card--title-row" style="display:flex;">
+    <div class="card--title t-typography-medium" style="padding-left:5px; align-items: center; display: flex; width: 100%; gap: 5px">
+      <LightningCharge class="filter-search-icon" style="color:rgba(152, 101, 35, 0.5); "/>{newSources.length} new files have been uploaded
     </div>
     <slot name="right">
-      <button
-          class="st-button icon fs-6"
-        >
-          <XIcon />
-        </button>
+      <button class="st-button icon fs-6" on:click={() => dispatch("dismiss")}>
+        <XIcon />
+      </button>
     </slot>
   </div>
+  <div style="padding: 4px 14px 12px;">
     <div class="card--row" style="padding-top: 5px">
       <span class="st-typography-body">
         New files matching source types in the current plan
       </span>
-      <div class="card--metadata-row">
     </div>
-  </div>
-  <div class="card--body st-typography-body">
-    {#each Object.keys(mappedSources) as sourceType}
-      <Collapse title={sourceType} tooltipContent={sourceType} defaultExpanded={true}>
-        {#each Object.keys(mappedSources[sourceType]) as derivationGroup}
-          <Collapse title={derivationGroup} tooltipContent={derivationGroup} defaultExpanded={true}>
-            {#each mappedSources[sourceType][derivationGroup] as source}
-              <div class="card--row">
-                <div class="card--metadata-row">
-                    <div class="card--date">
-                      <span class="st-typography-body">
-                        <p>{source.key}</p>
-                      </span>
-                      <span class="card--date-time-ago">
-                        <!-- {getTimeAgo(new Date(date), new Date(), Number.MAX_SAFE_INTEGER)} -->
-                        <p style:float="left" style:padding-top="0.1rem" style:padding-right="0.25rem" style:color="gray">
-                          {getTimeAgo(new Date(source.created_at), new Date(), Number.MAX_SAFE_INTEGER)}
-                        </p>
-                      </span>
-                    </div>
+    <div class="card--body st-typography-body">
+      {#each Object.keys(mappedSources) as sourceType}
+        <Collapse title={sourceType} tooltipContent={sourceType} defaultExpanded={false}>
+          {#each Object.keys(mappedSources[sourceType]) as derivationGroup}
+            <Collapse title={derivationGroup} tooltipContent={derivationGroup} defaultExpanded={false}>
+              {#each mappedSources[sourceType][derivationGroup] as source}
+                <div class="card--row">
+                  <div class="card--metadata-row">
+                      <div class="card--date">
+                        <span class="st-typography-body">
+                          <p>{source.key}</p>
+                        </span>
+                        <span class="card--date-time-ago">
+                          <!-- {getTimeAgo(new Date(date), new Date(), Number.MAX_SAFE_INTEGER)} -->
+                          <p style:float="left" style:padding-top="0.1rem" style:padding-right="0.25rem" style:color="gray">
+                            {getTimeAgo(new Date(source.created_at), new Date(), Number.MAX_SAFE_INTEGER)}
+                          </p>
+                        </span>
+                      </div>
+                  </div>
                 </div>
-              </div>
-            {/each}
-          </Collapse>
-        {/each}
-      </Collapse>
-    {/each}
+              {/each}
+            </Collapse>
+          {/each}
+        </Collapse>
+      {/each}
+    </div>
+    <div style="padding-top:5px">
+      <button style="border: 0px; width: 100px; background: rgb(235, 235, 235)" on:click={() => dispatch("dismiss")}>Dismiss</button>
+    </div>
+    <slot />
   </div>
-  <div style="padding-top:5px">
-    <button style="border: 0px; width: 100px; background: rgb(235, 235, 235)" on:click={() => dispatch("dismiss")}>Dismiss</button>
-  </div>
-  <slot />
 </div>
 
 <style>
   .card {
     background: var(--bg-color, rgba(245, 245, 245, 0.35));
-    border: 1px solid var(--border-color, rgba(249, 191, 29, 0.48));
+    border: 1px solid var(--border-color, rgba(152, 101, 35, 0.5));
     border-radius: 4px;
     color: var(--st-gray-70);
     cursor: pointer;
     display: flex;
     flex-direction: column;
-    padding: 4px 14px 12px;
+    /* padding: 4px 14px 12px; */
     text-align: left;
   }
 
@@ -115,7 +113,7 @@
   }
 
   .card--title-row {
-    background: var(--title-bg-color, rgba(255, 245, 100, 0.24));
+    background: var(--title-bg-color, rgb(254, 252, 234));
     padding: 4px 0px;
   }
 
