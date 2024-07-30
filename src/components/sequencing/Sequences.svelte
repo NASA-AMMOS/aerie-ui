109bf31ada4ec07f25e6a6841ecc6fef50917132
Cody Hansen <codyhansen92@gmail.com>
2024-07-30 12:21:19 -1000

    Rough prototype of workspaces

--- src/components/sequencing/Sequences.svelte
+++ src/components/sequencing/Sequences.svelte
@@ -1,18 +1,12 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-  import { goto } from '$app/navigation';
-  import { base } from '$app/paths';
   import { parcels, userSequences, userSequencesColumns } from '../../stores/sequencing';
   import type { User } from '../../types/app';
   import type { Parcel, UserSequence } from '../../types/sequencing';
-  import { permissionHandler } from '../../utilities/permissionHandler';
-  import { featurePermissions } from '../../utilities/permissions';
-  import Input from '../form/Input.svelte';
   import CssGrid from '../ui/CssGrid.svelte';
   import CssGridGutter from '../ui/CssGridGutter.svelte';
-  import Panel from '../ui/Panel.svelte';
-  import SectionTitle from '../ui/SectionTitle.svelte';
+  import WorkspaceTable from '../workspace/WorkspaceTable.svelte';
   import SequenceEditor from './SequenceEditor.svelte';
   import SequenceTable from './SequenceTable.svelte';
 
@@ -38,7 +32,10 @@
 </script>
 
 <CssGrid bind:columns={$userSequencesColumns}>
-  <Panel>
+  <WorkspaceTable {user}>
+    <SequenceTable {filterText} {user} on:sequenceSelected={onSequenceSelected} />
+  </WorkspaceTable>
+  <!--<Panel>
     <svelte:fragment slot="header">
       <SectionTitle>Sequences</SectionTitle>
 
@@ -55,15 +52,14 @@
           }}
           on:click={() => goto(`${base}/sequencing/new`)}
         >
-          New
+          New Sequence
         </button>
       </div>
     </svelte:fragment>
 
     <svelte:fragment slot="body">
-      <SequenceTable {filterText} {user} on:sequenceSelected={onSequenceSelected} />
     </svelte:fragment>
-  </Panel>
+  </Panel>-->
 
   <CssGridGutter track={1} type="column" />
 
