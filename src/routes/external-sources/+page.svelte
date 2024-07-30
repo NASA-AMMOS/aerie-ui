4f7be7599eef3431c413021e0fc706fbed8eb95f
jpvolosi <joseph.p.volosin@nasa.gov>
2024-07-30 12:08:35 -0400

    Add hook for null'ing external source creation error stores on page destory

--- src/routes/external-sources/+page.svelte
+++ src/routes/external-sources/+page.svelte
@@ -1,11 +1,22 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
+  import { onDestroy } from 'svelte';
   import PageTitle from '../../components/app/PageTitle.svelte';
   import ExternalSourceManager from '../../components/external-source/ExternalSourceManager.svelte';
+  import { createDerivationGroupError, createExternalSourceError, createExternalSourceEventTypeLinkError, createExternalSourceTypeError, derivationGroupPlanLinkError } from '../../stores/external-source';
   import type { PageData } from './$types';
 
   export let data: PageData;
+
+  onDestroy(() => {
+    // Clear the error stores when the page is destroyed
+    createExternalSourceError.set(null);
+    createExternalSourceTypeError.set(null);
+    createDerivationGroupError.set(null);
+    derivationGroupPlanLinkError.set(null);
+    createExternalSourceEventTypeLinkError.set(null);
+  });
 </script>
 
 <PageTitle title="External Sources" />
