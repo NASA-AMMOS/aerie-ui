79d6b7cbe28292ebf41df9c00688670778902e8d
psubram3 <pranav.subramanian@nasa.gov>
2024-08-02 14:51:56 -0700

    make tooltip disappear on timeline exit for Manager

--- src/components/timeline/Tooltip.svelte
+++ src/components/timeline/Tooltip.svelte
@@ -84,6 +84,18 @@
     }
   }
 
+  export function reset() {
+    activityDirectives = [];
+    externalEvents = [];
+    constraintResults = [];
+    gaps = [];
+    points = [];
+    row = null;
+    spans = [];
+
+    hide();
+  }
+
   export function hide() {
     select('.tooltip').style('opacity', 0).style('z-index', -1).html('');
     visible = false;
