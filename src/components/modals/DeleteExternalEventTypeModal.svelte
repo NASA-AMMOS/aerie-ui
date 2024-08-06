6eaa664a97ec441960cafb7c811a8f39283c9f22
psubram3 <pranav.subramanian@nasa.gov>
2024-08-06 14:16:21 -0700

    manual deletion of types

---
+++ src/components/modals/DeleteExternalEventTypeModal.svelte
@@ -0,0 +1,75 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import type { ExternalEventType } from '../../types/external-event';
+  import type { ExternalSourceWithResolvedNames } from '../../types/external-source';
+  import Modal from './Modal.svelte';
+  import ModalContent from './ModalContent.svelte';
+  import ModalFooter from './ModalFooter.svelte';
+  import ModalHeader from './ModalHeader.svelte';
+
+  export let height: number = 150;
+  export let eventType: ExternalEventType;
+  export let associatedSources: ExternalSourceWithResolvedNames[] = [];
+  export let width: number = 380;
+
+  const dispatch = createEventDispatcher<{
+    close: void;
+    confirm: void;
+  }>();
+
+  function onKeydown(event: KeyboardEvent) {
+    const { key } = event;
+    if (key === 'Enter') {
+      event.preventDefault();
+      dispatch('confirm');
+    }
+  }
+</script>
+
+<svelte:window on:keydown={onKeydown} />
+
+<Modal {height} {width}>
+  <ModalHeader on:close>
+    {#if associatedSources.length > 0}
+      External Event Type Cannot Be Deleted
+    {:else}
+      Delete External Event Type
+    {/if}
+  </ModalHeader>
+  <ModalContent>
+    {#if associatedSources.length > 0}
+      <span>
+        This External Event Type still contains the following sources which must be deleted first:
+        <hr style="border: 0px"/>
+        {#each associatedSources as source} 
+          <div style="padding-left:20px">
+            <i>
+              {source.key}
+            </i>
+          </div>
+        {/each}
+      </span>
+    {:else}
+      <span>
+        Are you sure you want to delete "{eventType.name}"?
+        <i>What is done cannot be undone.</i>
+      </span>
+    {/if}
+  </ModalContent>
+  <ModalFooter>
+    {#if associatedSources.length > 0}
+      <button class="st-button" on:click={() => dispatch('close')}>
+        Close
+      </button>
+    {:else}
+      <button class="st-button secondary" on:click={() => dispatch('close')}>
+        Cancel
+      </button>
+      <button class="st-button" on:click={() => dispatch('confirm')}>
+        Delete
+      </button>
+    {/if}
+  </ModalFooter>
+</Modal>
---
+++ src/components/modals/DeleteExternalSourceTypeModal.svelte
@@ -0,0 +1,74 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import type { DerivationGroup, ExternalSourceType } from '../../types/external-source';
+  import Modal from './Modal.svelte';
+  import ModalContent from './ModalContent.svelte';
+  import ModalFooter from './ModalFooter.svelte';
+  import ModalHeader from './ModalHeader.svelte';
+
+  export let height: number = 150;
+  export let sourceType: ExternalSourceType;
+  export let associatedDGs: DerivationGroup[] = [];
+  export let width: number = 380;
+
+  const dispatch = createEventDispatcher<{
+    close: void;
+    confirm: void;
+  }>();
+
+  function onKeydown(event: KeyboardEvent) {
+    const { key } = event;
+    if (key === 'Enter') {
+      event.preventDefault();
+      dispatch('confirm');
+    }
+  }
+</script>
+
+<svelte:window on:keydown={onKeydown} />
+
+<Modal {height} {width}>
+  <ModalHeader on:close>
+    {#if associatedDGs.length > 0}
+      External Source Type Cannot Be Deleted
+    {:else}
+      Delete External Source
+    {/if}
+  </ModalHeader>
+  <ModalContent>
+    {#if associatedDGs.length > 0}
+      <span>
+        The following Derivation Groups paired with this External Source Type still exist and must be deleted first:
+        <hr style="border: 0px"/>
+        {#each associatedDGs as dg} 
+          <div style="padding-left:20px">
+            <i>
+              {dg.name}
+            </i>
+          </div>
+        {/each}
+      </span>
+    {:else}
+      <span>
+        Are you sure you want to delete "{sourceType.name}"?
+        <i>What is done cannot be undone.</i>
+      </span>
+    {/if}
+  </ModalContent>
+  <ModalFooter>
+    {#if associatedDGs.length > 0}
+      <button class="st-button" on:click={() => dispatch('close')}>
+        Close
+      </button>
+    {:else}
+      <button class="st-button secondary" on:click={() => dispatch('close')}>
+        Cancel
+      </button>
+      <button class="st-button" on:click={() => dispatch('confirm')}>
+        Delete
+      </button>
+    {/if}
+  </ModalFooter>
+</Modal>
