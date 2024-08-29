eacc8cfa41b343aa1d3856d607ee8b6a4f88d473
psubram3 <pranav.subramanian@nasa.gov>
2024-08-29 11:55:38 -0700

    external event/activity compacted compatibility (I)

--- src/components/timeline/RowHeader.svelte
+++ src/components/timeline/RowHeader.svelte
@@ -24,6 +24,7 @@
   import { filterResourcesByLayer } from '../../utilities/timeline';
   import { tooltip } from '../../utilities/tooltip';
   import DropTarget from './DropTarget.svelte';
+  import Collapse from '../Collapse.svelte';
   import RowHeaderActivityTree from './RowHeaderActivityTree.svelte';
   import RowHeaderExternalEvent from './RowHeaderExternalEventTree.svelte';
   import RowHeaderMenu from './RowHeaderMenu.svelte';
@@ -158,8 +159,37 @@
           </button>
           <slot />
         </div>
-
-        {#if activityTree.length}
+        {#if activityTree.length && externalEventTree.length}
+        <Collapse
+          title="Activities"
+        >
+          <div class="activity-tree">
+            <RowHeaderActivityTree
+              {activityOptions}
+              {activityTree}
+              {selectedActivityDirectiveId}
+              {selectedSpanId}
+              on:activity-tree-node-change
+              on:mouseDown
+              on:dblClick
+            />
+          </div>
+        </Collapse>
+        <Collapse
+          title="External Events"
+        >
+          <div class="external-event-tree">
+            <RowHeaderExternalEvent
+              {externalEventOptions}
+              {externalEventTree}
+              {selectedExternalEventId}
+              on:external-event-tree-node-change
+              on:mouseDown
+              on:dblClick
+            />
+          </div>
+        </Collapse>
+        {:else if activityTree.length}
           <div class="activity-tree">
             <RowHeaderActivityTree
               {activityOptions}
@@ -388,5 +418,162 @@
 
   .external-event-tree {
     padding-left: 16px;
+  }
+
+
+
+
+
+
+
+  :global(.row-header-external-event-group) {
+    position: relative;
+  }
+
+  :global(.row-header-external-event-group.collapse > .collapse-header),
+  .row-header-external-event-group.leaf {
+    border-bottom: 1px solid var(--st-gray-30);
+    border-radius: 0px;
+    font-size: 10px;
+    letter-spacing: 0.1px;
+    padding: 0 !important;
+    padding-left: 4px !important;
+  }
+
+  :global(.row-header-external-event-group.collapse:not(.selected) > .collapse-header:hover),
+  .row-header-external-event-group.leaf:hover:not(.selected) {
+    background: var(--st-gray-20) !important;
+  }
+
+  :global(.row-header-external-event-group.collapse > .content.pad-content) {
+    margin-left: 16px !important;
+  }
+
+  :global(.row-header-external-event-group.collapse > .content) {
+    gap: 0px;
+  }
+
+  .row-header-external-event-group.leaf {
+    gap: 3px;
+    height: 32px;
+    justify-content: flex-start;
+    outline: none;
+    padding: 8px 0px 8px 0px;
+  }
+
+  .icon-group {
+    display: flex;
+    gap: 4px;
+  }
+
+  .title-metadata {
+    align-items: center;
+    color: var(--st-gray-50);
+    display: flex;
+    gap: 4px;
+  }
+
+  :global(.row-header-external-event-group.collapse .collapse-icon svg) {
+    color: var(--st-gray-40);
+  }
+
+  .selected,
+  :global(.collapse.selected > .collapse-header) {
+    background-color: #e3effd !important;
+  }
+
+  :global(.collapse.selected > .collapse-header *),
+  .row-header-external-event-group.selected,
+  .row-header-external-event-group.selected :global(svg) {
+    color: var(--st-utility-blue) !important;
+  }
+
+  :global(.row-header-external-event-group button.select) {
+    background: var(--st-gray-20);
+    height: 16px;
+    min-width: 16px;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 2px;
+    width: 16px;
+  }
+
+  :global(.row-header-external-event-group button.select:hover) {
+    background: var(--st-gray-20) !important;
+  }
+
+  :global(.row-header-external-event-group.collapse:has(> .collapse-header:focus-visible) > button.select),
+  :global(.row-header-external-event-group.collapse:has(> button.select:focus-visible) > button.select),
+  :global(.row-header-external-event-group.collapse:has(> .collapse-header:hover) > button.select),
+  :global(.row-header-external-event-group.collapse:has(> button.select:hover) > button.select) {
+    opacity: 1;
+  }
+
+  :global(.row-header-external-event-group .label) {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  :global(.row-header-activity-group) {
+    position: relative;
+  }
+
+  :global(.row-header-activity-group.collapse > .content.pad-content) {
+    margin-left: 16px !important;
+  }
+
+  :global(.row-header-activity-group.collapse > .content) {
+    gap: 0px;
+  }
+
+  .icon-group {
+    display: flex;
+    gap: 4px;
+  }
+
+  .title-metadata {
+    align-items: center;
+    color: var(--st-gray-50);
+    display: flex;
+    gap: 4px;
+  }
+
+  :global(.row-header-activity-group.collapse .collapse-icon svg) {
+    color: var(--st-gray-40);
+  }
+
+  .selected,
+  :global(.collapse.selected > .collapse-header) {
+    background-color: #e3effd !important;
+  }
+
+  :global(.row-header-activity-group button.select) {
+    background: var(--st-gray-20);
+    height: 16px;
+    min-width: 16px;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 2px;
+    width: 16px;
+  }
+
+  :global(.row-header-activity-group button.select:hover) {
+    background: var(--st-gray-20) !important;
+  }
+
+  :global(.row-header-activity-group.collapse:has(> .collapse-header:focus-visible) > button.select),
+  :global(.row-header-activity-group.collapse:has(> button.select:focus-visible) > button.select),
+  :global(.row-header-activity-group.collapse:has(> .collapse-header:hover) > button.select),
+  :global(.row-header-activity-group.collapse:has(> button.select:hover) > button.select) {
+    opacity: 1;
+  }
+
+  :global(.row-header-activity-group .label) {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
 </style>
