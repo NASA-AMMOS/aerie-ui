4f77ab23f103fa2e1e5990728499c14ec6b14262
JosephVolosin <joseph.p.volosin@nasa.gov>
2024-09-11 13:28:47 -0400

    Refactor UI design for derivation group & type management modal

---
+++ src/components/external-source/ExternalSourceTypeManagementTab.svelte
@@ -0,0 +1,20 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import { externalSourceTypes, getRowIdExternalSourceType } from '../../stores/external-source';
+  import type { DataGridColumnDef } from '../../types/data-grid';
+  import type { ExternalSourceType } from '../../types/external-source';
+  import DataGrid from '../ui/DataGrid/DataGrid.svelte';
+
+  export let estColumnDefs: DataGridColumnDef<ExternalSourceType>[] = [];
+  export let filterString: string = '';
+
+  let estDataGrid: DataGrid<ExternalSourceType>;
+</script>
+
+<DataGrid
+  bind:this={estDataGrid}
+  columnDefs={estColumnDefs}
+  rowData={$externalSourceTypes.filter(est => est.name.includes(filterString))}
+  getRowId={getRowIdExternalSourceType}
+/>
