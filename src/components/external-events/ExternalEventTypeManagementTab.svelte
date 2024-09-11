3852662b73ea6d002e5a580d8116284777f47e1e
JosephVolosin <joseph.p.volosin@nasa.gov>
2024-09-11 13:28:47 -0400

    Refactor UI design for derivation group & type management modal

---
+++ src/components/external-events/ExternalEventTypeManagementTab.svelte
@@ -0,0 +1,20 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import { externalEventTypes, getRowIdExternalEventType } from '../../stores/external-event';
+  import type { DataGridColumnDef } from '../../types/data-grid';
+  import type { ExternalEventType } from '../../types/external-event';
+  import DataGrid from '../ui/DataGrid/DataGrid.svelte';
+
+  export let eetColumnDefs: DataGridColumnDef<ExternalEventType>[] = [];
+  export let filterString: string = '';
+
+  let eetDataGrid: DataGrid<ExternalEventType>;
+</script>
+
+<DataGrid
+  bind:this={eetDataGrid}
+  columnDefs={eetColumnDefs}
+  rowData={$externalEventTypes.filter(eet => eet.name.includes(filterString))}
+  getRowId={getRowIdExternalEventType}
+/>
