b1fd7855711c6eecacb2440b70d3a40cd396a869
psubram3 <pranav.subramanian@nasa.gov>
2024-09-13 12:45:16 -0700

    Revert "update ExternalEventForm.svelte"
    This reverts commit 031559b8680c423b1dd2ed461b43f90883e89901.

--- src/components/external-events/ExternalEventForm.svelte
+++ src/components/external-events/ExternalEventForm.svelte
@@ -1,13 +1,12 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-  import { field } from '../../stores/form';
   import { plugins } from '../../stores/plugins';
   import type { ExternalEventDB } from '../../types/external-event';
   import type { ExternalEventProperty } from '../../types/external-event-property';
+  import { classNames } from '../../utilities/generic';
   import { formatDate } from '../../utilities/time';
   import Collapse from '../Collapse.svelte';
-  import DatePickerField from '../form/DatePickerField.svelte';
   import Input from '../form/Input.svelte';
   import Properties from './Properties.svelte';
 
@@ -16,7 +15,6 @@
 
   let formProperties: ExternalEventProperty[] = [];
 
-  $: startTimeField = field<string>(`${formatDate(new Date(externalEvent.start_time), $plugins.time.primary.format)}`);
   $: formProperties = Object.entries(externalEvent.properties).map(externalEvent => {
     return {
       name: externalEvent[0],
@@ -28,7 +26,7 @@
 <div class="external-event-form-container">
   {#if showHeader}
     <div class="external-event-header">
-      <div class="external-event-header-title">
+      <div class={classNames('external-event-header-title')}>
         <div class="external-event-header-title-value st-typography-medium">
           {externalEvent.pkey.key}
         </div>
@@ -44,13 +42,10 @@
           <input class="st-input w-100" disabled={true} name="event-type" value={externalEvent.pkey.event_type_name} />
         </Input>
 
-        <DatePickerField
-          layout="inline"
-          disabled={true}
-          field={startTimeField}
-          label={`Start Time (${$plugins.time.primary.label})`}
-          name="start-time"
-        />
+        <Input layout="inline">
+          {`Start Time (${$plugins.time.primary.label})`}
+          <input class="st-input w-100" disabled={true} name="start-time" value={formatDate(new Date(externalEvent.start_time), $plugins.time.primary.format)} />
+        </Input>
 
         <Input layout="inline">
           Duration
@@ -97,6 +92,10 @@
     overflow-y: auto;
   }
 
+  .external-event-directive-definition {
+    padding: 0.5rem;
+  }
+
   .external-event-header {
     align-items: center;
     background: var(--st-gray-10);
@@ -108,12 +107,31 @@
     padding-left: 8px;
   }
 
+  .external-event-header-icons {
+    align-items: center;
+    display: flex;
+  }
+
+  .external-event-error-rollup {
+    display: inline;
+    font-style: normal;
+  }
+
+  .external-event-header-title-placeholder,
+  .external-event-header-title-value {
+    word-break: break-word;
+  }
+
   .external-event-header-title-value {
     overflow: hidden;
     padding: 4px 0px;
     text-overflow: ellipsis;
     white-space: nowrap;
     word-break: break-all;
+  }
+
+  .external-event-header-title-placeholder {
+    padding: 4px 8px;
   }
 
   .external-event-header-title {
@@ -126,5 +144,43 @@
   .external-event-header-title :global(fieldset) {
     padding: 0;
     width: 100%;
+  }
+
+  .external-event-header-title-edit-button:hover {
+    background-color: var(--st-white);
+  }
+
+  .external-event-header-title--editing {
+    gap: 8px;
+    padding: 0;
+    width: 100%;
+  }
+
+  .external-event-header-changelog {
+    border: 1px solid transparent;
+    display: flex;
+    width: 24px;
+  }
+
+  .external-event-header-changelog:hover {
+    color: #007bff;
+  }
+
+  .revision-preview-header {
+    align-items: center;
+    background-color: #e6e6ff;
+    border-bottom: 1px solid #c4c6ff;
+    border-top: 1px solid #c4c6ff;
+    display: flex;
+    flex-shrink: 0;
+    justify-content: space-between;
+    padding: 4px 8px;
+    padding-left: 8px;
+  }
+
+  .annotations {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
   }
 </style>
