2f281ab4837f0c8d1dca9734e503aa824f0e0ee8
psubram3 <pranav.subramanian@nasa.gov>
2024-09-18 12:15:50 -0700

    force events to show up
    an earlier refactor of gql.ts hid everything!

--- src/utilities/gql.ts
+++ src/utilities/gql.ts
@@ -1561,7 +1561,7 @@ const gql = {
           derivation_group_name: {_eq: $derivationGroupName}
         }
       ) {
-        ${Queries.EXTERNAL_EVENT} {
+        external_events {
           external_event_type {
             name
           }
@@ -1805,8 +1805,8 @@ const gql = {
       ${Queries.PLAN_DERIVATION_GROUP}(where: {plan_id: {_eq: $plan_id}}) {
         ${Queries.DERIVATION_GROUP} {
           ${Queries.EXTERNAL_SOURCES} {
-            ${Queries.EXTERNAL_EVENT} {
-              ${Queries.EXTERNAL_EVENT_TYPES} {
+            external_events {
+              external_event_type {
                 name
               }
             }
