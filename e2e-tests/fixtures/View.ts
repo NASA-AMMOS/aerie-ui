7da3c1c6fe640fe208404cb7c80479c82604963e
bduran <bryan.duran@jpl.nasa.gov>
2024-09-18 13:42:38 -0700

    update dependencies

--- e2e-tests/fixtures/View.ts
+++ e2e-tests/fixtures/View.ts
@@ -78,7 +78,7 @@ export class View {
     const nameColumnHeader = await this.table.getByRole('columnheader', { name: 'Name' });
     await nameColumnHeader.hover();
 
-    const filterIcon = await nameColumnHeader.locator('.ag-icon-menu');
+    const filterIcon = await nameColumnHeader.locator('.ag-icon-filter');
     await expect(filterIcon).toBeVisible();
     await filterIcon.click();
     await this.page.locator('.ag-popup').getByRole('textbox', { name: 'Filter Value' }).first().fill(viewName);
