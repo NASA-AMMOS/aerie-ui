5a98f624dc1f74bb98ccd3bc12a6cf56470608e3
JosephVolosin <joseph.p.volosin@nasa.gov>
2024-09-18 15:21:01 -0400

    Add basic support for external events in the 'Activity & Resource Types' menu item

---
+++ src/components/ExternalEventTypeList.svelte
@@ -0,0 +1,26 @@
+<svelte:options immutable={true} />
+
+<script lang="ts">
+  import { selectedPlanDerivationGroupEventTypes } from '../stores/external-source';
+  import type { ExternalEventType } from '../types/external-event';
+  import type { TimelineItemType } from '../types/timeline';
+  import TimelineItemList from './TimelineItemList.svelte';
+
+  let externalEventTypes: ExternalEventType[] = [];
+
+  $: $selectedPlanDerivationGroupEventTypes.map(eventType => { eventType });
+
+  function getFilterValueFromItem(item: TimelineItemType) {
+    return item.name;
+  }
+</script>
+
+<TimelineItemList
+  items={externalEventTypes}
+  chartType="external-event"
+  typeName="external event type"
+  typeNamePlural="External Event Types"
+  filterOptions={$selectedPlanDerivationGroupEventTypes.map(t => ({ label: t, value: t }))}
+  filterName="External Event Type"
+  {getFilterValueFromItem}
+/>
