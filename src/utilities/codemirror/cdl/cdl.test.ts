0f916b7156adc2b6acb49ba5076001b838788b87
Chet Joswig <cjoswig@yahoo.com>
2024-09-20 09:58:10 -0700

    wip_cdl_parser

--- src/utilities/codemirror/cdl/cdl.test.ts
+++ src/utilities/codemirror/cdl/cdl.test.ts
@@ -1,8 +1,8 @@
 import type { SyntaxNode } from '@lezer/common';
+import type { FswCommand, Header } from '@nasa-jpl/aerie-ampcs';
 import { readFileSync } from 'fs';
 import { assert, describe, expect, test } from 'vitest';
-import { CdlLanguage, TOKEN_ERROR } from './cdl';
-// import { parseStem } from './cdlParser';
+import { CdlLanguage, parseHeader, parseStem, TOKEN_ERROR } from './cdl';
 
 describe('parse cdl dictionary', () => {
   const dictionaryPath = '/Users/joswig/Documents/Aerie/Juno/JNO_6.0.4_REV_M00';
@@ -16,52 +16,32 @@ describe('parse cdl dictionary', () => {
     assertNoErrorNodes(contents, true);
   });
 
-  //   test('stem parsing', () => {
-  //     const input = `
-  // STEM : CMD_TEST1 ( ccode : 1, data : 2 )
-
-  // cmd-type : INST
-
-  // READ ARGUMENT arg_1
-  // READ ARGUMENT arg_2
-
-  // ccode := [ 1 ] '07E9' HEX
-
-  // data := crm_opcode_0x07//crm_macro//[1920]'000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'BINARY
-
-  // !@ ATTACHMENT : desc
-  // !@    "Terminates macro definition."
-  // !@ END ATTACHMENT
-
-  // !@ ATTACHMENT : tci
-  // !@    "No specific telemetry measurement(s) identified."
-  // !@ END ATTACHMENT
-
-  // !@ ATTACHMENT : con
-  // !@    "Routed to the same message ID in FSW as the CRM_PASS_THRU command.  This command will be rejected if no macro is being defined."
-  // !@ END ATTACHMENT
-
-  // !@ ATTACHMENT : imm
-  // !@    "Yes"
-  // !@ END ATTACHMENT
-
-  // !@ ATTACHMENT : res
-  // !@    "See MRO-03-0080 Flight Rules and Constraints"
-  // !@ END ATTACHMENT
-
-  // !@ ATTACHMENT : seq
-  // !@    "Yes"
-  // !@ END ATTACHMENT
-
-  // END STEM
-
-  // STEM : CRM_ROM_BOOT ( ccode : 16, data : 1936 )
-
-  //     `;
-
-  //     // printNodes(input);
-  //     assertNoErrorNodes(input);
-  //   });
+  test('print tokens', () => {
+    const parsed = CdlLanguage.parser.parse(contents);
+    let cursor = parsed.cursor();
+    do {
+      const { node } = cursor;
+      if (node.name !== TOKEN_ERROR) {
+        console.log(node.type.name);
+      }
+    } while (cursor.next());
+    cursor = parsed.cursor();
+    let header: Header | undefined = undefined;
+    const stems: FswCommand[] = [];
+    do {
+      const { node } = cursor;
+      switch (node.type.name) {
+        case 'Header':
+          header = parseHeader(contents, node);
+          break;
+        case 'Processing_routine':
+          stems.push(parseStem(contents, node));
+          break;
+      }
+    } while (cursor.next());
+    stems.forEach(stem => console.log(stem.stem));
+    console.log(header);
+  });
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -77,8 +57,8 @@ function assertNoErrorNodes(input: string, printPrefix?: boolean) {
     const { node } = cursor;
     if (printPrefix) {
       if (node.type.name === TOKEN_ERROR) {
-        printNodes(input.substring(0, node.to + 10));
-        console.log(input.substring(0, node.to));
+        // printNodes(input.substring(0, node.to + 10));
+        console.log(`${input.substring(node.to - 60, node.to)}\nXXXXXX\n${input.substring(node.to, node.to + 60)}`);
       }
     }
     assert.notStrictEqual(node.type.name, TOKEN_ERROR);
@@ -89,17 +69,4 @@ function assertNoErrorNodes(input: string, printPrefix?: boolean) {
 function printNode(input: string, node: SyntaxNode) {
   console.log(`${node.type.name}[${node.from}.${node.to}] --> '''${nodeContents(input, node)}'''`);
   // console.log(`${node.type.name}`);
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function printNodes(input: string, filter?: (name: string) => boolean, limit?: number) {
-  const cursor = CdlLanguage.parser.parse(input).cursor();
-  let count = 0;
-  do {
-    const { node } = cursor;
-    if (!filter || filter(node.type.name)) {
-      printNode(input, node);
-    }
-    count++;
-  } while (cursor.next() && count < (limit ?? Infinity));
 }
--- src/utilities/codemirror/cdl/cdl.ts
+++ src/utilities/codemirror/cdl/cdl.ts
@@ -1,5 +1,7 @@
 import { LanguageSupport, LRLanguage } from '@codemirror/language';
+import type { SyntaxNode } from '@lezer/common';
 import { styleTags } from '@lezer/highlight';
+import type { FswCommand, Header } from '@nasa-jpl/aerie-ampcs';
 import { parser } from './cdl.grammar';
 
 export const TOKEN_ERROR = '⚠';
@@ -15,6 +17,32 @@ export const CdlLanguage = LRLanguage.define({
 
 export function setupCdlLanguageSupport() {
   return new LanguageSupport(CdlLanguage);
+}
+
+export function parseHeader(_contents: string, _header: SyntaxNode): Header {
+  return {
+    mission_name: getNodeText(_contents, _header.getChild('Project')?.getChild('String')),
+    schema_version: 'cdl', // TODO: see if available in dictionary
+    spacecraft_ids: [0], // TODO: see if available in dictionary
+    version: getNodeText(_contents, _header.getChild('Version')?.getChild('String')),
+  };
+}
+
+export function parseStem(contents: string, stem: SyntaxNode): FswCommand {
+  return {
+    argumentMap: {},
+    arguments: [],
+    description: '',
+    stem: getNodeText(contents, stem.getChild('Stem_subroutine')?.getChild('Stem_start')?.getChild('Stem_name')),
+    type: 'fsw_command',
+  };
+}
+
+function getNodeText(contents: string, node: SyntaxNode | null | undefined): string {
+  if (!node) {
+    return '';
+  }
+  return contents.slice(node.from, node.to);
 }
 
 setupCdlLanguageSupport();
