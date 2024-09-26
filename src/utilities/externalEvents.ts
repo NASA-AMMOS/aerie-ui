e83e2976fd6822a4757ba6a77681663dd594f577
JosephVolosin <joseph.p.volosin@nasa.gov>
2024-09-26 16:37:49 -0400

    Simplify external event row ID functions
    Use  strings instead of + concatenation, re-use getRowIdExternalEvent for getRowIdExternalEventWhole

--- src/utilities/externalEvents.ts
+++ src/utilities/externalEvents.ts
@@ -8,21 +8,11 @@ import type {
 
 // External Event Row/Hash Functions
 export function getRowIdExternalEventWhole(externalEvent: ExternalEvent): ExternalEventId {
-  return (
-    externalEvent.pkey.derivation_group_name +
-    externalEvent.pkey.source_key +
-    externalEvent.pkey.event_type_name +
-    externalEvent.pkey.key
-  );
+  return getRowIdExternalEvent(externalEvent.pkey);
 }
 
 export function getRowIdExternalEvent(externalEventPkey: ExternalEventPkey): ExternalEventId {
-  return (
-    externalEventPkey.derivation_group_name +
-    externalEventPkey.source_key +
-    externalEventPkey.event_type_name +
-    externalEventPkey.key
-  );
+  return `${externalEventPkey.derivation_group_name}${externalEventPkey.source_key}${externalEventPkey.event_type_name}${externalEventPkey.key}`;
 }
 
 export function getRowIdExternalEventType(externalEventType: ExternalEventType): string {
@@ -31,15 +21,15 @@ export function getRowIdExternalEventType(externalEventType: ExternalEventType):
 
 // External Source Row/Hash Functions
 export function getRowIdExternalSourceSlim(externalSourceSlim: ExternalSourceSlim): string {
-  return externalSourceSlim.pkey.derivation_group_name + externalSourceSlim.pkey.key;
+  return `${externalSourceSlim.pkey.derivation_group_name}${externalSourceSlim.pkey.key}`;
 }
 
 export function getRowIdExternalSource(externalSourcePkey: ExternalSourcePkey): string {
-  return externalSourcePkey.derivation_group_name + externalSourcePkey.key;
+  return `${externalSourcePkey.derivation_group_name}${externalSourcePkey.key}`;
 }
 
 export function getRowIdDerivationGroup(derivationGroup: DerivationGroup): string {
-  return `${derivationGroup.name}:${derivationGroup.source_type_name}`;
+  return `${derivationGroup.name}${derivationGroup.source_type_name}`;
 }
 
 export function getRowIdExternalSourceType(externalSourceType: ExternalSourceType): string {
