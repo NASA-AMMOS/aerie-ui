5ff3f33e184f9f470188621e08d8b21cbe66d826
psubram3 <pranav.subramanian@nasa.gov>
2024-09-27 12:25:22 -0700

    filter presented events by plan bounds

--- src/stores/external-event.ts
+++ src/stores/external-event.ts
@@ -4,8 +4,9 @@ import { derived, writable, type Readable, type Writable } from 'svelte/store';
 import type { ExternalEvent, ExternalEventDB, ExternalEventId, ExternalEventType } from '../types/external-event';
 import { getRowIdExternalEventWhole } from '../utilities/externalEvents';
 import gql from '../utilities/gql';
-import { convertDurationToMs, convertUTCtoMs } from '../utilities/time';
+import { convertDoyToYmd, convertDurationToMs, convertUTCtoMs } from '../utilities/time';
 import { selectedPlanDerivationGroupNames } from './external-source';
+import { plan } from './plan';
 import { gqlSubscribable } from './subscribable';
 import { viewUpdateGrid } from './views';
 
@@ -14,12 +15,11 @@ export const creatingExternalEventType: Writable<boolean> = writable(false);
 export const createExternalEventTypeError: Writable<string | null> = writable(null);
 
 /* Subscriptions. */
-export const externalEvents = gqlSubscribable<ExternalEvent[]>(
+export const externalEventsRaw = gqlSubscribable<{ external_event: ExternalEventDB }[] | null | undefined>(
   gql.SUB_PLAN_EXTERNAL_EVENTS_DERIVATION_GROUP,
   { derivation_group_names: selectedPlanDerivationGroupNames },
   [],
-  null,
-  transformExternalEvents,
+  null
 );
 export const externalEventTypes = gqlSubscribable<ExternalEventType[]>(gql.SUB_EXTERNAL_EVENT_TYPES, {}, [], null);
 
@@ -27,6 +27,45 @@ export const externalEventTypes = gqlSubscribable<ExternalEventType[]>(gql.SUB_E
 export const selectedExternalEventId: Writable<ExternalEventId | null> = writable(null);
 
 /* Derived. */
+export const externalEvents: Readable<ExternalEvent[]> = derived(
+  [externalEventsRaw, plan],
+  ([$externalEventsRaw, $plan]) => {
+    const completeExternalEvents: ExternalEvent[] = [];
+    if ($externalEventsRaw !== null && $externalEventsRaw !== undefined) {
+      // get plan bounds in an easily comparable format. The explicit strings are extreme bounds in case of a null plan.
+      const planStartTime = convertUTCtoMs(convertDoyToYmd($plan?.start_time_doy ?? "1970-001T00:00:00Z") ?? "1970-01-01T00:00:00Z");
+      const planEndTime = convertUTCtoMs(convertDoyToYmd($plan?.end_time_doy ?? "2100-001T00:00:00Z") ?? "2100-01-01T00:00:00Z");
+
+      $externalEventsRaw.forEach(e => {
+        // stored in this fashion as a byproduct of the GQL subscription query.
+        const externalEvent = e.external_event;
+
+        // check event is in plan bounds
+        const externalEventStartTime = convertUTCtoMs(externalEvent.start_time);
+        const externalEventEndTime = externalEventStartTime + convertDurationToMs(externalEvent.duration);
+
+        if ((externalEventStartTime >= planStartTime && externalEventStartTime <= planEndTime) ||
+            (externalEventEndTime <= planEndTime && externalEventEndTime >= planStartTime)) {
+          completeExternalEvents.push({
+            duration: externalEvent.duration,
+            duration_ms: convertDurationToMs(externalEvent.duration),
+            pkey: {
+              derivation_group_name: externalEvent.derivation_group_name,
+              event_type_name: externalEvent.event_type_name,
+              key: externalEvent.key,
+              source_key: externalEvent.source_key,
+            },
+            properties: externalEvent.properties,
+            start_ms: convertUTCtoMs(externalEvent.start_time),
+            start_time: externalEvent.start_time,
+          });
+        }
+      });
+    }
+    return completeExternalEvents;
+  }
+);
+
 export const externalEventsMap: Readable<Dictionary<ExternalEvent>> = derived(externalEvents, $externalEvents => {
   return keyBy($externalEvents, getRowIdExternalEventWhole);
 });
@@ -62,30 +101,4 @@ export function selectExternalEvent(
   } else {
     selectedExternalEventId.set(null);
   }
-}
-
-function transformExternalEvents(
-  externalEvents: { external_event: ExternalEventDB }[] | null | undefined,
-): ExternalEvent[] {
-  const completeExternalEvents: ExternalEvent[] = [];
-  if (externalEvents !== null && externalEvents !== undefined) {
-    return externalEvents.map(e => {
-      // stored in this fashion as a byproduct of the GQL subscription query.
-      const externalEvent = e.external_event;
-      return {
-        duration: externalEvent.duration,
-        duration_ms: convertDurationToMs(externalEvent.duration),
-        pkey: {
-          derivation_group_name: externalEvent.derivation_group_name,
-          event_type_name: externalEvent.event_type_name,
-          key: externalEvent.key,
-          source_key: externalEvent.source_key,
-        },
-        properties: externalEvent.properties,
-        start_ms: convertUTCtoMs(externalEvent.start_time),
-        start_time: externalEvent.start_time,
-      };
-    });
-  }
-  return completeExternalEvents;
 }
