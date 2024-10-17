ed10b54cb2aa4f7e6ad859ff2b7b371e6f043496
JosephVolosin <joseph.p.volosin@nasa.gov>
2024-10-17 10:36:36 -0400

    Retire selectedPlanDerivationGroupEventTypes for
    Allows filtering of any event type on the timeline, not just those linked to the plan - mirroring how activity types work and improving performance

--- src/components/ExternalEventTypeList.svelte
+++ src/components/ExternalEventTypeList.svelte
@@ -1,16 +1,9 @@
 <svelte:options immutable={true} />
 
 <script lang="ts">
-  import { selectedPlanDerivationGroupEventTypes } from '../stores/external-source';
-  import type { ExternalEventType } from '../types/external-event';
+  import { externalEventTypes } from '../stores/external-event';
   import type { TimelineItemType } from '../types/timeline';
   import TimelineItemList from './TimelineItemList.svelte';
-
-  let externalEventTypes: ExternalEventType[] = [];
-
-  $: externalEventTypes = $selectedPlanDerivationGroupEventTypes.map(eventType => {
-    return { name: eventType };
-  });
 
   function getFilterValueFromItem(item: TimelineItemType) {
     return item.name;
@@ -18,11 +11,11 @@
 </script>
 
 <TimelineItemList
-  items={externalEventTypes}
+  items={$externalEventTypes}
   chartType="externalEvent"
   typeName="externalEvent"
   typeNamePlural="External Events"
-  filterOptions={$selectedPlanDerivationGroupEventTypes.map(t => ({ label: t, value: t }))}
+  filterOptions={$externalEventTypes.map(t => ({ label: t.name, value: t.name }))}
   filterName="Event Type"
   {getFilterValueFromItem}
 />
