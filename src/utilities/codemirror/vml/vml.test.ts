31ea81bdbeeeac6f4782efc5bdf434d6b48c7a18
Chet Joswig <cjoswig@yahoo.com>
2024-10-22 20:23:45 -0700

    unit tests for vml tree navigation

--- src/utilities/codemirror/vml/vml.test.ts
+++ src/utilities/codemirror/vml/vml.test.ts
@@ -1,6 +1,6 @@
-import type { SyntaxNode } from '@lezer/common';
+import type { SyntaxNode, TreeCursor } from '@lezer/common';
 import type { FswCommandArgumentInteger } from '@nasa-jpl/aerie-ampcs';
-import { assert, describe, expect, it } from 'vitest';
+import { assert, describe, expect, it, test } from 'vitest';
 import { VmlLanguage } from './vml';
 import { vmlBlockLibraryToCommandDictionary } from './vmlBlockLibrary';
 import {
@@ -9,6 +9,7 @@ import {
   RULE_ASSIGNMENT,
   RULE_BLOCK,
   RULE_BODY,
+  RULE_CALL_PARAMETERS,
   RULE_COMMON_FUNCTION,
   RULE_FUNCTION,
   RULE_FUNCTION_NAME,
@@ -22,6 +23,7 @@ import {
   RULE_VM_MANAGEMENT,
   TOKEN_ERROR,
 } from './vmlConstants';
+import { getArgumentPosition, VmlCommandInfoMapper } from './vmlTreeUtils';
 
 // In versions of VML prior to 2.1, explicit time tags were required on every statement
 // confirm what version we're using
@@ -430,7 +432,97 @@ END_MODULE
       assertNoErrorNodes(input, true);
     }
   });
+
+  describe('vml command info mapper', () => {
+    const input = `MODULE
+
+RELATIVE_SEQUENCE vnv
+FLAGS AUTOEXECUTE AUTOUNLOAD
+BODY
+;initialize variables
+R00:00:01.00 ISSUE CMD_001 "ENUM_B",FALSE_VM_CONST
+R00:00:01.00 ISSUE CMD_002 1,2,3,4
+
+
+;TEST CASE 1
+R00:01:00 CALL pay_spawn "seis_pwr_on_r01_1"
+
+END_BODY
+END_MODULE
+`;
+    const parsed = VmlLanguage.parser.parse(input);
+    const vmlCommandInfoMapper = new VmlCommandInfoMapper();
+    const timeTaggedNodes: SyntaxNode[] = Array.from(
+      filterNodes(parsed.cursor(), (node: SyntaxNode) => node.name === RULE_TIME_TAGGED_STATEMENT),
+    );
+
+    test('time tagged count', () => {
+      expect(timeTaggedNodes.length).toBe(3);
+    });
+
+    test.each([
+      [0, 'CMD_001'],
+      [1, 'CMD_002'],
+      [2, null],
+    ])('command %i name %s', (statementIndex: number, expectedStem: string | null) => {
+      const nameNode = vmlCommandInfoMapper.getNameNode(timeTaggedNodes[statementIndex]);
+      if (expectedStem) {
+        expect(nameNode).toBeDefined();
+        expect(nameNode!.name).toBe(RULE_FUNCTION_NAME);
+        expect(nodeContents(input, nameNode!)).toBe(expectedStem);
+      }
+    });
+
+    test.each([
+      [0, 2],
+      [1, 4],
+      [2, 1],
+    ])('command %i argument count %s', (statementIndex: number, argCount: number) => {
+      const argContainer = vmlCommandInfoMapper.getArgumentNodeContainer(timeTaggedNodes[statementIndex]);
+      expect(argContainer).toBeDefined();
+      expect(argContainer!.name).toBe(RULE_CALL_PARAMETERS);
+      expect(vmlCommandInfoMapper.getArgumentsFromContainer(argContainer!).length).toBe(argCount);
+    });
+
+    test.each([
+      ['"ENUM_B"', 0],
+      ['FALSE_VM_CONST', 1],
+      ['1', 0],
+      ['2', 1],
+      ['3', 2],
+      ['4', 3],
+    ])("argument value '%s' index %i", (argumentValue: string, argIndexInCommand: number) => {
+      const argValueFilter = (node: SyntaxNode) => nodeContents(input, node) === argumentValue;
+      const argNode = filterNodes(parsed.cursor(), argValueFilter).next().value as SyntaxNode;
+      expect(argNode).toBeDefined();
+      expect(getArgumentPosition(argNode)).toBe(argIndexInCommand);
+    });
+
+    test('arg insert position', () => {
+      const uniqArgValue = 'FALSE_VM_CONST';
+      const inputPosition = input.indexOf(uniqArgValue) + uniqArgValue.length;
+      const argValueFilter = (node: SyntaxNode) => nodeContents(input, node) === uniqArgValue;
+      const argNode = filterNodes(parsed.cursor(), argValueFilter).next().value as SyntaxNode;
+      const cmdNode = vmlCommandInfoMapper.getContainingCommand(argNode);
+      expect(vmlCommandInfoMapper.getArgumentAppendPosition(cmdNode)).toBe(inputPosition);
+    });
+  });
 });
+
+function* filterNodes(cursor: TreeCursor, filter?: (name: SyntaxNode) => boolean): Generator<SyntaxNode> {
+  do {
+    const { node } = cursor;
+    if (!filter || filter(node)) {
+      yield node;
+    }
+  } while (cursor.next());
+}
+
+function printNodes(input: string): void {
+  for (const node of filterNodes(VmlLanguage.parser.parse(input).cursor())) {
+    printNode(input, node);
+  }
+}
 
 function allowedInVmlVersion(minVmlVersion: string): boolean {
   const minVersionArray = minVmlVersion.split('.').map(part => parseInt(part, 10));
@@ -442,18 +534,18 @@ function allowedInVmlVersion(minVmlVersion: string): boolean {
   return true;
 }
 
-function wrapInModule(s: string) {
+function wrapInModule(vmlBody: string): string {
   return `
 MODULE
 BLOCK placeholder
 BODY
-${s}
+${vmlBody}
 END_BODY
 END_MODULE
   `;
 }
 
-function assertNoErrorNodes(input: string, printPrefix?: boolean) {
+function assertNoErrorNodes(input: string, printPrefix?: boolean): void {
   const cursor = VmlLanguage.parser.parse(input).cursor();
   do {
     const { node } = cursor;
@@ -467,20 +559,10 @@ function assertNoErrorNodes(input: string, printPrefix?: boolean) {
   } while (cursor.next());
 }
 
-function nodeContents(input: string, node: SyntaxNode) {
+function nodeContents(input: string, node: SyntaxNode): string {
   return input.substring(node.from, node.to);
 }
 
-function printNode(input: string, node: SyntaxNode) {
+function printNode(input: string, node: SyntaxNode): void {
   console.log(`${node.type.name}[${node.from}.${node.to}] --> '${nodeContents(input, node)}'`);
-}
-
-function printNodes(input: string, filter?: (name: string) => boolean) {
-  const cursor = VmlLanguage.parser.parse(input).cursor();
-  do {
-    const { node } = cursor;
-    if (!filter || filter(node.type.name)) {
-      printNode(input, node);
-    }
-  } while (cursor.next());
 }
